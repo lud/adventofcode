491e9bc6c69374ea2d0a4fd322510d1e2a9e28ea
lud <ludovic@demblans.com>
2020-12-06 11:31:00 +0100

    d6

---
+++ lib/solutions/2020/day_6.ex
@@ -0,0 +1,47 @@
+defmodule Aoe.Y20.Day6 do
+  alias Aoe.Input, warn: false
+
+  @type input_path :: binary
+  @type file :: input_path | %Aoe.Input.FakeFile{}
+  @type part :: :part_one | :part_two
+  @type input :: binary | File.Stream.t()
+  @type problem :: any
+
+  @spec read_file!(file, part) :: input
+  def read_file!(file, _part) do
+    Input.read!(file)
+    # Input.stream!(file)
+    # Input.stream_file_lines(file, trim: true)
+  end
+
+  @spec parse_input!(input, part) :: problem
+  def parse_input!(input, :part_one) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&String.replace(&1, "\n", ""))
+  end
+
+  def parse_input!(input, :part_two) do
+    input
+    |> String.split("\n\n")
+    |> Enum.map(fn group ->
+      group
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_charlist/1)
+    end)
+  end
+
+  @spec part_one(problem) :: integer
+  def part_one(problem) do
+    problem
+    |> Enum.map(fn group -> group |> String.graphemes() |> Enum.uniq() |> length end)
+    |> Enum.sum()
+  end
+
+  @spec part_two(problem) :: integer
+  def part_two(problem) do
+    problem
+    |> Enum.map(fn group -> group |> Enum.reduce(&(&1 -- &1 -- &2)) |> length end)
+    |> Enum.sum()
+  end
+end
---
+++ test/2020/day_6_test.exs
@@ -0,0 +1,91 @@
+defmodule Aoe.Y20.Day6Test do
+  use ExUnit.Case, async: true
+
+  alias Aoe.Y20.Day6, as: Solution, warn: false
+  alias Aoe.Input, warn: false
+
+  # To run the test, run the following command:
+  #
+  #     mix test test/2020/day_6_test.exs
+  #
+  # To run the solution
+  #
+  #     mix aoe.run --year 2020 --day 6 --part 1
+  #
+  # Use sample input: 
+  #
+  #     {:ok, path} = Input.resolve(2020, 6, "sample-1")
+  #
+
+  test "verify 2020/6 part_one - samples" do
+    problem =
+      """
+      abc
+
+      a
+      b
+      c
+
+      ab
+      ac
+
+      a
+      a
+      a
+      a
+
+      b
+      """
+      |> Input.as_file()
+      |> Solution.read_file!(:part_one)
+      |> Solution.parse_input!(:part_one)
+
+    expected = 11
+    assert expected == Solution.part_one(problem)
+  end
+
+  test "verify 2020/6 part_two - samples" do
+    problem =
+      """
+      abc
+
+      a
+      b
+      c
+
+      ab
+      ac
+
+      a
+      a
+      a
+      a
+
+      b
+      """
+      |> Input.as_file()
+      |> Solution.read_file!(:part_two)
+      |> Solution.parse_input!(:part_two)
+
+    expected = 6
+    assert expected == Solution.part_two(problem)
+  end
+
+  # Once your part one was successfully sumbitted, you may uncomment this test
+  # to ensure your implementation was not altered when you implement part two.
+
+  @part_one_solution 6335
+
+  test "verify 2020/6 part one" do
+    assert {:ok, @part_one_solution} == Aoe.run(2020, 6, :part_one)
+  end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  @part_two_solution 3392
+
+  test "verify 2020/6 part two" do
+    assert {:ok, @part_two_solution} == Aoe.run(2020, 6, :part_two)
+  end
+end
