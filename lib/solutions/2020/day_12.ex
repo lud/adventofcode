2ca83e1373d7c9654dae99492b58f7621de46617
lud <ludovic@demblans.com>
2020-12-12 11:37:28 +0100

    d12

---
+++ lib/solutions/2020/day_12.ex
@@ -0,0 +1,84 @@
+defmodule Aoe.Y20.Day12 do
+  alias Aoe.Input, warn: false
+
+  @type input_path :: binary
+  @type file :: input_path | %Aoe.Input.FakeFile{}
+  @type part :: :part_one | :part_two
+  @type input :: binary | File.Stream.t()
+  @type problem :: any
+
+  @spec read_file!(file, part) :: input
+  def read_file!(file, _part) do
+    Input.stream_file_lines(file, trim: true)
+  end
+
+  @spec parse_input!(input, part) :: problem
+  def parse_input!(input, _part) do
+    input
+    |> Stream.map(&parse_line/1)
+  end
+
+  def parse_line(<<char>> <> int) do
+    {char, String.to_integer(int)}
+  end
+
+  def part_one(problem) do
+    problem
+    |> Enum.reduce({0, 0, ?E}, &reduce_p1/2)
+    |> manhattan()
+  end
+
+  defp manhattan({x, y, _}) do
+    manhattan({x, y})
+  end
+
+  defp manhattan({x, y}) do
+    abs(x) + abs(y)
+  end
+
+  defp reduce_p1({?N, v}, {x, y, dir}), do: {x, y - v, dir}
+  defp reduce_p1({?S, v}, {x, y, dir}), do: {x, y + v, dir}
+  defp reduce_p1({?E, v}, {x, y, dir}), do: {x + v, y, dir}
+  defp reduce_p1({?W, v}, {x, y, dir}), do: {x - v, y, dir}
+  defp reduce_p1({side, amt}, {x, y, dir}) when side in [?L, ?R], do: {x, y, turn(dir, side, amt)}
+  defp reduce_p1({?F, v}, {x, y, dir}), do: reduce_p1({dir, v}, {x, y, dir})
+
+  @turn %{
+    ?N => %{?L => ?W, ?R => ?E},
+    ?S => %{?L => ?E, ?R => ?W},
+    ?W => %{?L => ?S, ?R => ?N},
+    ?E => %{?L => ?N, ?R => ?S}
+  }
+
+  defp turn(dir, _, 0) do
+    dir
+  end
+
+  defp turn(dir, side, amt) do
+    turn(@turn[dir][side], side, amt - 90)
+  end
+
+  def part_two(problem) do
+    problem
+    |> Enum.reduce({{0, 0}, {10, -1}, ?E}, &reduce_p2/2)
+    |> elem(0)
+    |> manhattan()
+  end
+
+  defp reduce_p2({?N, v}, {ship, {wp_x, wp_y}, dir}), do: {ship, {wp_x, wp_y - v}, dir}
+  defp reduce_p2({?S, v}, {ship, {wp_x, wp_y}, dir}), do: {ship, {wp_x, wp_y + v}, dir}
+  defp reduce_p2({?E, v}, {ship, {wp_x, wp_y}, dir}), do: {ship, {wp_x + v, wp_y}, dir}
+  defp reduce_p2({?W, v}, {ship, {wp_x, wp_y}, dir}), do: {ship, {wp_x - v, wp_y}, dir}
+
+  defp reduce_p2({side, 180}, {ship, {wp_x, wp_y}, dir}) when side in [?L, ?R],
+    do: {ship, {-wp_x, -wp_y}, dir}
+
+  defp reduce_p2({side, 270}, state) when side in [?L, ?R],
+    do: reduce_p2({if(side == ?L, do: ?R, else: ?L), 90}, state)
+
+  defp reduce_p2({?L, 90}, {ship, {wp_x, wp_y}, dir}), do: {ship, {wp_y, -wp_x}, dir}
+  defp reduce_p2({?R, 90}, {ship, {wp_x, wp_y}, dir}), do: {ship, {-wp_y, wp_x}, dir}
+
+  defp reduce_p2({?F, v}, {{s_x, s_y}, {wp_x, wp_y} = wp, dir}),
+    do: {{s_x + v * wp_x, s_y + v * wp_y}, wp, dir}
+end
---
+++ test/2020/day_12_test.exs
@@ -0,0 +1,67 @@
+defmodule Aoe.Y20.Day12Test do
+  use ExUnit.Case, async: true
+
+  alias Aoe.Y20.Day12, as: Solution, warn: false
+  alias Aoe.Input, warn: false
+
+  # To run the test, run the following command:
+  #
+  #     mix test test/2020/day_12_test.exs
+  #
+  # To run the solution
+  #
+  #     mix aoe.run --year 2020 --day 12 --part 1
+  #
+  # Use sample input: 
+  #
+  #     {:ok, path} = Input.resolve(2020, 12, "sample-1")
+  #
+
+  @sample_1 """
+  F10
+  N3
+  F7
+  R90
+  F11
+  """
+
+  test "verify 2020/12 part_one - samples" do
+    problem =
+      @sample_1
+      |> Input.as_file()
+      |> Solution.read_file!(:part_one)
+      |> Solution.parse_input!(:part_one)
+
+    expected = 25
+    assert expected == Solution.part_one(problem)
+  end
+
+  test "verify 2020/12 part_two - samples" do
+    problem =
+      @sample_1
+      |> Input.as_file()
+      |> Solution.read_file!(:part_two)
+      |> Solution.parse_input!(:part_two)
+
+    expected = 286
+    assert expected == Solution.part_two(problem)
+  end
+
+  # Once your part one was successfully sumbitted, you may uncomment this test
+  # to ensure your implementation was not altered when you implement part two.
+
+  @part_one_solution 757
+
+  test "verify 2020/12 part one" do
+    assert {:ok, @part_one_solution} == Aoe.run(2020, 12, :part_one)
+  end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  # @part_two_solution CHANGE_ME
+  #
+  # test "verify 2020/12 part two" do
+  #   assert {:ok, @part_two_solution} == Aoe.run(2020, 12, :part_two)
+  # end
+end
