bf636f90da297744edfa24046ede1e2b5c5d73ac
lud <ludovic@demblans.com>
2020-12-17 10:27:04 +0100

    d17

---
+++ lib/solutions/2020/day_17.ex
@@ -0,0 +1,191 @@
+defmodule Aoe.Y20.Day17 do
+  alias Aoe.Input, warn: false
+  import Enum, only: [with_index: 1]
+
+  @type input_path :: binary
+  @type file :: input_path | %Aoe.Input.FakeFile{}
+  @type part :: :part_one | :part_two
+  @type input :: binary | File.Stream.t()
+  @type problem :: any
+
+  @spec read_file!(file, part) :: input
+  def read_file!(file, _part) do
+    file
+    |> Input.stream_file_lines(trim: true)
+    |> Enum.to_list()
+  end
+
+  @spec parse_input!(input, part) :: problem
+  def parse_input!(input, :part_one) do
+    z = 0
+
+    map =
+      for {row, y} <- with_index(input),
+          {col, x} <- with_index(String.to_charlist(row)),
+          into: %{},
+          do: {{x, y, z}, parse_char(col)}
+
+    max_x = Enum.max(map |> Enum.map(fn {{x, _, _}, _} -> x end))
+    min_x = Enum.min(map |> Enum.map(fn {{x, _, _}, _} -> x end))
+    max_y = Enum.max(map |> Enum.map(fn {{_, y, _}, _} -> y end))
+    min_y = Enum.min(map |> Enum.map(fn {{_, y, _}, _} -> y end))
+
+    Map.put(map, :domain, {min_x, max_x, min_y, max_y, 0, 0})
+  end
+
+  def parse_input!(input, :part_two) do
+    z = 0
+    w = 0
+
+    map =
+      for {row, y} <- with_index(input),
+          {col, x} <- with_index(String.to_charlist(row)),
+          into: %{},
+          do: {{x, y, z, w}, parse_char(col)}
+
+    max_x = Enum.max(map |> Enum.map(fn {{x, _, _, _}, _} -> x end))
+    min_x = Enum.min(map |> Enum.map(fn {{x, _, _, _}, _} -> x end))
+    max_y = Enum.max(map |> Enum.map(fn {{_, y, _, _}, _} -> y end))
+    min_y = Enum.min(map |> Enum.map(fn {{_, y, _, _}, _} -> y end))
+
+    Map.put(map, :domain, {min_x, max_x, min_y, max_y, 0, 0, 0, 0})
+  end
+
+  @active 1
+  @inactive 0
+
+  defp parse_char(?#), do: @active
+  defp parse_char(?.), do: @inactive
+
+  def part_one(problem) do
+    map = apply_rules_p1(problem, 6)
+
+    for {_, v} <- map, v == @active do
+      1
+    end
+    |> Enum.sum()
+  end
+
+  def part_two(problem) do
+    map = apply_rules_p2(problem, 6)
+
+    for {_, v} <- map, v == @active do
+      1
+    end
+    |> Enum.sum()
+  end
+
+  defp apply_rules_p1(map, 0) do
+    map
+  end
+
+  defp apply_rules_p1(map, turn) do
+    %{domain: domain} = map
+    {min_x, max_x, min_y, max_y, min_z, max_z} = expand_domain_once(domain)
+
+    changes =
+      for x <- min_x..max_x, y <- min_y..max_y, z <- min_z..max_z, reduce: [] do
+        changes ->
+          coords = {x, y, z}
+          anc = active_neighbours_count(map, coords)
+          current = Map.get(map, coords, @inactive)
+
+          case {current, anc} do
+            {@active, c} when c in [2, 3] -> changes
+            {@active, _} -> [{coords, @inactive} | changes]
+            {@inactive, 3} -> [{coords, @active} | changes]
+            {@inactive, _} -> changes
+          end
+      end
+
+    new_domain =
+      Enum.reduce(changes, domain, fn
+        {coords, @active}, acc -> expand_domain(acc, coords)
+        {_coords, @inactive}, acc -> acc
+      end)
+
+    map
+    |> Map.put(:domain, new_domain)
+    |> Map.merge(Map.new(changes))
+    |> apply_rules_p1(turn - 1)
+  end
+
+  defp apply_rules_p2(map, 0) do
+    map
+  end
+
+  defp apply_rules_p2(map, turn) do
+    %{domain: domain} = map
+    {min_x, max_x, min_y, max_y, min_z, max_z, min_w, max_w} = expand_domain_once(domain)
+
+    changes =
+      for x <- min_x..max_x,
+          y <- min_y..max_y,
+          z <- min_z..max_z,
+          w <- min_w..max_w,
+          reduce: [] do
+        changes ->
+          coords = {x, y, z, w}
+          anc = active_neighbours_count(map, coords)
+          current = Map.get(map, coords, @inactive)
+
+          case {current, anc} do
+            {@active, c} when c in [2, 3] -> changes
+            {@active, _} -> [{coords, @inactive} | changes]
+            {@inactive, 3} -> [{coords, @active} | changes]
+            {@inactive, _} -> changes
+          end
+      end
+
+    new_domain =
+      Enum.reduce(changes, domain, fn
+        {coords, @active}, acc -> expand_domain(acc, coords)
+        {_coords, @inactive}, acc -> acc
+      end)
+
+    map
+    |> Map.put(:domain, new_domain)
+    |> Map.merge(Map.new(changes))
+    |> apply_rules_p2(turn - 1)
+  end
+
+  defp expand_domain_once({min_x, max_x, min_y, max_y, min_z, max_z}) do
+    {min_x - 1, max_x + 1, min_y - 1, max_y + 1, min_z - 1, max_z + 1}
+  end
+
+  defp expand_domain_once({min_x, max_x, min_y, max_y, min_z, max_z, min_w, max_w}) do
+    {min_x - 1, max_x + 1, min_y - 1, max_y + 1, min_z - 1, max_z + 1, min_w - 1, max_w + 1}
+  end
+
+  defp expand_domain({min_x, max_x, min_y, max_y, min_z, max_z}, {x, y, z}) do
+    {min(min_x, x), max(max_x, x), min(min_y, y), max(max_y, y), min(min_z, z), max(max_z, z)}
+  end
+
+  defp expand_domain({min_x, max_x, min_y, max_y, min_z, max_z, min_w, max_w}, {x, y, z, w}) do
+    {min(min_x, x), max(max_x, x), min(min_y, y), max(max_y, y), min(min_z, z), max(max_z, z),
+     min(min_w, w), max(max_w, w)}
+  end
+
+  defp active_neighbours_count(map, {x, y, z} = coords) do
+    for nx <- (x - 1)..(x + 1),
+        ny <- (y - 1)..(y + 1),
+        nz <- (z - 1)..(z + 1),
+        {nx, ny, nz} != coords,
+        reduce: 0 do
+      acc ->
+        Map.get(map, {nx, ny, nz}, 0) + acc
+    end
+  end
+
+  defp active_neighbours_count(map, {x, y, z, w} = coords) do
+    for nx <- (x - 1)..(x + 1),
+        ny <- (y - 1)..(y + 1),
+        nz <- (z - 1)..(z + 1),
+        nw <- (w - 1)..(w + 1),
+        {nx, ny, nz, nw} != coords,
+        reduce: 0 do
+      acc ->
+        Map.get(map, {nx, ny, nz, nw}, 0) + acc
+    end
+  end
+end
---
+++ test/2020/day_17_test.exs
@@ -0,0 +1,65 @@
+defmodule Aoe.Y20.Day17Test do
+  use ExUnit.Case, async: true
+
+  alias Aoe.Y20.Day17, as: Solution, warn: false
+  alias Aoe.Input, warn: false
+
+  # To run the test, run the following command:
+  #
+  #     mix test test/2020/day_17_test.exs
+  #
+  # To run the solution
+  #
+  #     mix aoe.run --year 2020 --day 17 --part 1
+  #
+  # Use sample input: 
+  #
+  #     {:ok, path} = Input.resolve(2020, 17, "sample-1")
+  #
+
+  @sample_1 """
+  .#.
+  ..#
+  ###
+  """
+
+  test "verify 2020/17 part_one - samples" do
+    problem =
+      @sample_1
+      |> Input.as_file()
+      |> Solution.read_file!(:part_one)
+      |> Solution.parse_input!(:part_one)
+
+    expected = 112
+    assert expected == Solution.part_one(problem)
+  end
+
+  test "verify 2020/17 part_two - samples" do
+    problem =
+      @sample_1
+      |> Input.as_file()
+      |> Solution.read_file!(:part_two)
+      |> Solution.parse_input!(:part_two)
+
+    expected = 848
+    assert expected == Solution.part_two(problem)
+  end
+
+  # Once your part one was successfully sumbitted, you may uncomment this test
+  # to ensure your implementation was not altered when you implement part two.
+
+  @part_one_solution 380
+
+  test "verify 2020/17 part one" do
+    assert {:ok, @part_one_solution} == Aoe.run(2020, 17, :part_one)
+  end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  @part_two_solution 2332
+
+  test "verify 2020/17 part two" do
+    assert {:ok, @part_two_solution} == Aoe.run(2020, 17, :part_two)
+  end
+end
