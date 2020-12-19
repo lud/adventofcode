fe26df376eaafbb23318c9f12bf85154305fe170
lud <ludovic@demblans.com>
2020-12-19 12:58:48 +0100

    day 19 ugly

---
+++ test/2020/day_19_test.exs
@@ -0,0 +1,124 @@
+defmodule Aoe.Y20.Day19Test do
+  use ExUnit.Case, async: true
+
+  alias Aoe.Y20.Day19, as: Solution, warn: false
+  alias Aoe.Input, warn: false
+
+  # To run the test, run the following command:
+  #
+  #     mix test test/2020/day_19_test.exs
+  #
+  # To run the solution
+  #
+  #     mix aoe.run --year 2020 --day 19 --part 1
+  #
+  # Use sample input: 
+  #
+  #     {:ok, path} = Input.resolve(2020, 19, "sample-1")
+  #
+
+  @sample_1 """
+  0: 4 1 5
+  1: 2 3 | 3 2
+  2: 4 4 | 5 5
+  3: 4 5 | 5 4
+  4: "a"
+  5: "b"
+
+  ababbb
+  bababa
+  abbbab
+  aaabbb
+  aaaabbb
+  """
+
+  test "verify 2020/19 part_one - samples" do
+    problem =
+      @sample_1
+      |> Input.as_file()
+      |> Solution.read_file!(:part_one)
+      |> Solution.parse_input!(:part_one)
+
+    expected = 2
+    assert expected == Solution.part_one(problem)
+  end
+
+  @sample_2 """
+  42: 9 14 | 10 1
+  9: 14 27 | 1 26
+  10: 23 14 | 28 1
+  1: "a"
+  11: 42 31
+  5: 1 14 | 15 1
+  19: 14 1 | 14 14
+  12: 24 14 | 19 1
+  16: 15 1 | 14 14
+  31: 14 17 | 1 13
+  6: 14 14 | 1 14
+  2: 1 24 | 14 4
+  0: 8 11
+  13: 14 3 | 1 12
+  15: 1 | 14
+  17: 14 2 | 1 7
+  23: 25 1 | 22 14
+  28: 16 1
+  4: 1 1
+  20: 14 14 | 1 15
+  3: 5 14 | 16 1
+  27: 1 6 | 14 18
+  14: "b"
+  21: 14 1 | 1 14
+  25: 1 1 | 1 14
+  22: 14 14
+  8: 42
+  26: 14 22 | 1 20
+  18: 15 15
+  7: 14 5 | 1 21
+  24: 14 1
+
+  abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+  bbabbbbaabaabba
+  babbbbaabbbbbabbbbbbaabaaabaaa
+  aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+  bbbbbbbaaaabbbbaaabbabaaa
+  bbbababbbbaaaaaaaabbababaaababaabab
+  ababaaaaaabaaab
+  ababaaaaabbbaba
+  baabbaaaabbaaaababbaababb
+  abbbbabbbbaaaababbbbbbaaaababb
+  aaaaabbaabaaaaababaa
+  aaaabbaaaabbaaa
+  aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+  babaaabbbaaabaababbaabababaaab
+  aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+  """
+
+  test "verify 2020/19 part_two - samples" do
+    problem =
+      @sample_2
+      |> Input.as_file()
+      |> Solution.read_file!(:part_two)
+      |> Solution.parse_input!(:part_two)
+
+    expected = 12
+    assert expected == Solution.part_two(problem)
+  end
+
+  # Once your part one was successfully sumbitted, you may uncomment this test
+  # to ensure your implementation was not altered when you implement part two.
+
+  @part_one_solution 173
+
+  test "verify 2020/19 part one" do
+    assert {:ok, @part_one_solution} == Aoe.run(2020, 19, :part_one)
+  end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  @part_two_solution 367
+
+  test "verify 2020/19 part two" do
+    assert {:ok, @part_two_solution} == Aoe.run(2020, 19, :part_two)
+  end
+end
