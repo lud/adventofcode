912b72c55017982a0a88fcd2eb01d5adceecfa4a
lud <ludovic@demblans.com>
2020-12-20 12:43:22 +0100

    d20p1

---
+++ lib/solutions/2020/day_20.ex
@@ -0,0 +1,110 @@
+defmodule Aoe.Y20.Day20 do
+  alias Aoe.Input, warn: false
+
+  @type input_path :: binary
+  @type file :: input_path | %Aoe.Input.FakeFile{}
+  @type part :: :part_one | :part_two
+  @type input :: binary | File.Stream.t()
+  @type problem :: any
+
+  @spec read_file!(file, part) :: input
+  def read_file!(file, _part) do
+    Input.read!(file)
+  end
+
+  @spec parse_input!(input, part) :: problem
+  def parse_input!(input, _part) do
+    input
+    |> String.split(~r/\n{2,}/, trim: true)
+    |> Enum.map(&parse_tile/1)
+    |> Map.new()
+    |> IO.inspect(label: "map")
+  end
+
+  defdelegate stc(str), to: String, as: :to_charlist
+
+  defp parse_tile("Tile " <> rest) do
+    [header | rows] = String.split(rest, "\n", trim: true)
+    {id, ":"} = Integer.parse(header)
+    {id, rows}
+  end
+
+  def part_one(map, return_data? \\ false) do
+    signmap =
+      map
+      |> Enum.map(&tile_to_signatures/1)
+      |> Map.new()
+
+    sign2neighbours = signmap |> Enum.reduce(%{}, &register_signatures/2)
+
+    neighbourcounts =
+      sign2neighbours
+      |> Enum.reduce(%{}, &count_neighbours/2)
+
+    corners =
+      neighbourcounts
+      |> Enum.filter(fn {id, count} -> count == 2 end)
+
+    if return_data? do
+      {signmap, sign2neighbours, neighbourcounts, corners}
+    else
+      corners
+      |> Enum.map(&elem(&1, 0))
+      |> Enum.reduce(&(&1 * &2))
+    end
+  end
+
+  defp tile_to_signatures({id, rows}) do
+    rows = rows |> Enum.map(&stc/1)
+    top_row = rows |> hd
+    bottom_row = rows |> List.last()
+    left_col = rows |> Enum.map(&hd/1)
+    rigth_col = rows |> Enum.map(&List.last/1)
+
+    # tile =
+    #   for {row, y} <- Enum.with_index(rows),
+    #       {char, x} <- Enum.with_index(stc(row)),
+    #       into: %{} do
+    #     {{x, y}, char}
+    #   end
+
+    # {id,
+    #  %{
+    #    top: signature(top_row),
+    #    bottom: signature(bottom_row),
+    #    left: signature(left_col),
+    #    right: signature(rigth_col)
+    #  }}
+    {id, [signature(top_row), signature(bottom_row), signature(left_col), signature(rigth_col)]}
+  end
+
+  defp register_signatures({id, signatures}, acc) do
+    Enum.reduce(signatures, acc, fn sign, acc ->
+      Map.update(acc, sign, [id], &[id | &1])
+    end)
+  end
+
+  defp count_neighbours({_sign, ids}, acc) do
+    case ids do
+      # This side signature corresponds to two tiles, so each tile is the neighbour of the other, we add +1 neighbour to each tile id
+      [id_a, id_b] ->
+        acc
+        |> Map.update(id_a, 1, &(&1 + 1))
+        |> Map.update(id_b, 1, &(&1 + 1))
+
+      # This side is a border
+      [alone] ->
+        acc
+    end
+  end
+
+  defp signature(chars) do
+    rev = :lists.reverse(chars)
+    [a, b] = :lists.sort([chars, rev])
+    :lists.flatten([a, '|', b])
+  end
+
+  def part_two(problem) do
+    {signmap, sign2neighbours, neighbourcounts, corners} = part_one(problem, true)
+  end
+end
---
+++ test/2020/day_20_test.exs
@@ -0,0 +1,169 @@
+defmodule Aoe.Y20.Day20Test do
+  use ExUnit.Case, async: true
+
+  alias Aoe.Y20.Day20, as: Solution, warn: false
+  alias Aoe.Input, warn: false
+
+  # To run the test, run the following command:
+  #
+  #     mix test test/2020/day_20_test.exs
+  #
+  # To run the solution
+  #
+  #     mix aoe.run --year 2020 --day 20 --part 1
+  #
+  # Use sample input: 
+  #
+  #     {:ok, path} = Input.resolve(2020, 20, "sample-1")
+  #
+
+  @sample_1 """
+  Tile 2311:
+  ..##.#..#.
+  ##..#.....
+  #...##..#.
+  ####.#...#
+  ##.##.###.
+  ##...#.###
+  .#.#.#..##
+  ..#....#..
+  ###...#.#.
+  ..###..###
+
+  Tile 1951:
+  #.##...##.
+  #.####...#
+  .....#..##
+  #...######
+  .##.#....#
+  .###.#####
+  ###.##.##.
+  .###....#.
+  ..#.#..#.#
+  #...##.#..
+
+  Tile 1171:
+  ####...##.
+  #..##.#..#
+  ##.#..#.#.
+  .###.####.
+  ..###.####
+  .##....##.
+  .#...####.
+  #.##.####.
+  ####..#...
+  .....##...
+
+  Tile 1427:
+  ###.##.#..
+  .#..#.##..
+  .#.##.#..#
+  #.#.#.##.#
+  ....#...##
+  ...##..##.
+  ...#.#####
+  .#.####.#.
+  ..#..###.#
+  ..##.#..#.
+
+  Tile 1489:
+  ##.#.#....
+  ..##...#..
+  .##..##...
+  ..#...#...
+  #####...#.
+  #..#.#.#.#
+  ...#.#.#..
+  ##.#...##.
+  ..##.##.##
+  ###.##.#..
+
+  Tile 2473:
+  #....####.
+  #..#.##...
+  #.##..#...
+  ######.#.#
+  .#...#.#.#
+  .#########
+  .###.#..#.
+  ########.#
+  ##...##.#.
+  ..###.#.#.
+
+  Tile 2971:
+  ..#.#....#
+  #...###...
+  #.#.###...
+  ##.##..#..
+  .#####..##
+  .#..####.#
+  #..#.#..#.
+  ..####.###
+  ..#.#.###.
+  ...#.#.#.#
+
+  Tile 2729:
+  ...#.#.#.#
+  ####.#....
+  ..#.#.....
+  ....#..#.#
+  .##..##.#.
+  .#.####...
+  ####.#.#..
+  ##.####...
+  ##..#.##..
+  #.##...##.
+
+  Tile 3079:
+  #.#.#####.
+  .#..######
+  ..#.......
+  ######....
+  ####.#..#.
+  .#...#.##.
+  #.#####.##
+  ..#.###...
+  ..#.......
+  ..#.###...
+  """
+
+  test "verify 2020/20 part_one - samples" do
+    problem =
+      @sample_1
+      |> Input.as_file()
+      |> Solution.read_file!(:part_one)
+      |> Solution.parse_input!(:part_one)
+
+    expected = 20_899_048_083_289
+    assert expected == Solution.part_one(problem)
+  end
+
+  test "verify 2020/20 part_two - samples" do
+    problem =
+      @sample_1
+      |> Input.as_file()
+      |> Solution.read_file!(:part_two)
+      |> Solution.parse_input!(:part_two)
+
+    expected = CHANGE_ME
+    assert expected == Solution.part_two(problem)
+  end
+
+  # Once your part one was successfully sumbitted, you may uncomment this test
+  # to ensure your implementation was not altered when you implement part two.
+
+  @part_one_solution 21_599_955_909_991
+
+  test "verify 2020/20 part one" do
+    assert {:ok, @part_one_solution} == Aoe.run(2020, 20, :part_one)
+  end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  # @part_two_solution CHANGE_ME
+  #
+  # test "verify 2020/20 part two" do
+  #   assert {:ok, @part_two_solution} == Aoe.run(2020, 20, :part_two)
+  # end
+end
