54a6f4f1cb6b4540d696994d5f0956fb6e0660e3
lud <ludovic@demblans.com>
2020-12-23 23:18:01 +0100

    d23

--- lib/solutions/2020/day_23.ex
+++ lib/solutions/2020/day_23.ex
@@ -21,11 +21,9 @@ defmodule Aoe.Y20.Day23 do
   end
 
   def part_one(cups, moves_count \\ 100) do
-    IO.puts("INITIAL: #{inspect(cups)}")
     max_cup = Enum.max(cups)
     [cur | _] = cups
     cmap = init_map(cups, max_cup)
-    cmap |> IO.inspect(label: "cmap")
 
     move(cmap, cur, moves_count, 1, max_cup)
     |> to_list(1)
@@ -35,15 +33,10 @@ defmodule Aoe.Y20.Day23 do
   end
 
   def part_two(cups) do
-    max_cup = Enum.max(cups)
-    cups = cups ++ Enum.to_list(10..1_000_000)
+    max_cup = 1_000_000
+    cups = cups ++ Enum.to_list(10..max_cup)
     [cur | _] = cups
-    cmap = init_map(cups, 1_000_000)
-    Map.fetch!(cmap, 1_000_000) |> IO.inspect(label: "Map.fetch!(cmap, 1000000)")
-
-    cmap
-    |> to_list(cur, 40)
-    |> IO.inspect(label: "initial partial", charlists: :as_lists)
+    cmap = init_map(cups, max_cup)
 
     cmap = move(cmap, cur, 10_000_000, 1, max_cup)
     next_a = cmap[1]
@@ -52,9 +45,7 @@ defmodule Aoe.Y20.Day23 do
   end
 
   defp move(cmap, cur, max_move, nmove, max_cup) when nmove <= max_move do
-    # Process.sleep(1000)
-
-    if rem(nmove, 1000) == 0 do
+    if rem(nmove, 100_000) == 0 do
       IO.puts("move #{nmove}: #{cur}")
     end
 
@@ -62,23 +53,9 @@ defmodule Aoe.Y20.Day23 do
     dest = find_dest(cur - 1, {a, b, c}, max_cup)
     dest_next = Map.fetch!(cmap, dest)
 
-    # IO.puts("current: #{cur}, move #{a},#{b},#{c} after #{dest}, before #{dest_next}")
-    [cur: cur, a: a, b: b, c: c, d: d, dest: dest, dest_next: dest_next]
-    |> IO.inspect(label: "binding(partial)")
-
-    cmap =
-      cmap
-      |> Map.merge(%{dest => a, c => dest_next, cur => d})
-
-    # cmap
-    # |> to_list(cur, 40)
-    # |> IO.inspect(label: "list", charlists: :as_lists)
-
-    # cmap
-    # |> to_list(1_000_000, 40)
-    # |> IO.inspect(label: "xend", charlists: :as_lists)
-
-    move(cmap, d, max_move, nmove + 1, max_cup)
+    cmap
+    |> Map.merge(%{dest => a, c => dest_next, cur => d})
+    |> move(d, max_move, nmove + 1, max_cup)
   end
 
   defp move(cmap, _cur, _max_move, _nmove, _max_cup) do
@@ -109,16 +86,16 @@ defmodule Aoe.Y20.Day23 do
     {a, b, c, d}
   end
 
-  defp find_dest(0, _ignored, max_cup) do
-    find_dest(max_cup, _ignored, max_cup)
+  defp find_dest(0, ignored, max_cup) do
+    find_dest(max_cup, ignored, max_cup)
   end
 
-  defp find_dest(cur, {a, b, c}, max_cup) when cur in [a, b, c] do
-    find_dest(cur - 1, {a, b, c}, max_cup)
+  defp find_dest(candidate, {a, b, c}, max_cup) when candidate in [a, b, c] do
+    find_dest(candidate - 1, {a, b, c}, max_cup)
   end
 
-  defp find_dest(cur, _ingored, _max_cup) do
-    cur
+  defp find_dest(candidate, _ingored, _max_cup) do
+    candidate
   end
 
   # define a map where the cups are the keys, and the values are tne next cup
--- test/2020/day_23_test.exs
+++ test/2020/day_23_test.exs
@@ -67,8 +67,8 @@ defmodule Aoe.Y20.Day23Test do
   # You may also implement a test to validate the part two to ensure that you
   # did not broke your shared modules when implementing another problem.
 
-  # @part_two_solution CHANGE_ME
-  #
+  # @part_two_solution 689_500_518_476
+
   # test "verify 2020/23 part two" do
   #   assert {:ok, @part_two_solution} == Aoe.run(2020, 23, :part_two)
   # end
