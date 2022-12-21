5137f43aa0186585b61a0bd111ec7dcda7821ba5
lud <ludovic@demblans.com>
2022-12-21 14:44:50 +0100

    Day 21 ok

---
+++ lib/solutions/2022/day_21.ex
@@ -0,0 +1,191 @@
+defmodule Aoe.Y22.Day21 do
+  alias Aoe.Input, warn: false
+
+  @type input_path :: binary
+  @type file :: input_path | %Aoe.Input.FakeFile{}
+  @type part :: :part_one | :part_two
+  @type input :: binary | File.Stream.t()
+  @type problem :: any
+
+  @spec read_file!(file, part) :: input
+  def read_file!(file, _part) do
+    Input.stream_file_lines(file, trim: true)
+  end
+
+  @spec parse_input!(input, part) :: problem
+  def parse_input!(input, _part) do
+    input |> Map.new(&parse_line/1)
+  end
+
+  defp parse_line(<<label::binary-size(4), ": ", math::binary>>) do
+    case Integer.parse(math) do
+      {n, ""} -> {label, {:val, n}}
+      _ -> {label, {:op, parse_op(math)}}
+    end
+  end
+
+  defp parse_op(<<left::binary-size(4), " ", op::binary-size(1), " ", right::binary-size(4)>>) do
+    {String.to_atom(op), left, right}
+  end
+
+  def part_one(problem) do
+    solved =
+      Enum.filter(problem, fn
+        {_, {:val, _}} -> true
+        _ -> false
+      end)
+      |> Map.new(fn {k, {:val, n}} -> {k, n} end)
+
+    solved = resolve(problem, ["root"], solved)
+    solved["root"]
+  end
+
+  defp resolve(problem, [cur | stack], solved) do
+    case Map.fetch!(problem, cur) do
+      {:op, {op, left, right}} ->
+        case {Map.fetch(solved, left), Map.fetch(solved, right)} do
+          {{:ok, l}, {:ok, r}} -> resolve(problem, stack, Map.put(solved, cur, calc(op, l, r)))
+          {:error, {:ok, _}} -> resolve(problem, [left, cur | stack], solved)
+          {{:ok, _}, :error} -> resolve(problem, [right, cur | stack], solved)
+          {:error, :error} -> resolve(problem, [left, right, cur | stack], solved)
+        end
+
+      {:val, n} ->
+        resolve(problem, stack, Map.put(solved, cur, n))
+    end
+  end
+
+  defp resolve(_, [], solved) do
+    solved
+  end
+
+  defp calc(:+, a, b), do: a + b
+  defp calc(:-, a, b), do: a - b
+  defp calc(:*, a, b), do: a * b
+  defp calc(:/, a, b), do: div(a, b)
+
+  def part_two(problem) do
+    solved =
+      Enum.filter(problem, fn
+        {_, {:val, _}} -> true
+        _ -> false
+      end)
+      |> Map.new(fn {k, {:val, n}} -> {k, n} end)
+      |> Map.delete("humn")
+
+    {reduced, _} =
+      problem
+      |> Map.update!("humn", fn {:val, _} -> :you end)
+      |> Map.update!("root", fn {:op, {_, l, r}} -> {:EQUAL, l, r} end)
+      |> Map.new()
+      |> preloop(solved)
+
+    # now one of the two equality checks is hopefully a value
+    {:EQUAL, left, right} = Map.fetch!(reduced, "root")
+
+    {label, value} =
+      case Map.fetch!(reduced, left) do
+        {:val, v} ->
+          {right, v}
+
+        {:op, _} ->
+          {:val, v} = Map.fetch!(reduced, right)
+          {left, v}
+      end
+
+    # now we know that label value must be equal to value
+
+    equation = nest_ops(reduced, label)
+
+    run_equation(equation, value)
+  end
+
+  defp run_equation({:+, {_, _, _} = next, int}, value) when is_integer(int) do
+    run_equation(next, value - int)
+  end
+
+  defp run_equation({:/, {_, _, _} = next, int}, value) when is_integer(int) do
+    run_equation(next, value * int)
+  end
+
+  defp run_equation({:+, int, {_, _, _} = next}, value) when is_integer(int) do
+    run_equation(next, value - int)
+  end
+
+  defp run_equation({:*, int, {_, _, _} = next}, value) when is_integer(int) do
+    run_equation(next, div(value, int))
+  end
+
+  defp run_equation({:*, {_, _, _} = next, int}, value) when is_integer(int) do
+    run_equation(next, div(value, int))
+  end
+
+  defp run_equation({:-, int, {_, _, _} = next}, value) when is_integer(int) do
+    run_equation(next, int - value)
+  end
+
+  defp run_equation({:-, {_, _, _} = next, int}, value) when is_integer(int) do
+    run_equation(next, value + int)
+  end
+
+  defp run_equation({op, :you, v}, value) do
+    case op do
+      :- -> value + v
+    end
+  end
+
+  defp nest_ops(reduced, label) when is_binary(label) do
+    case Map.fetch!(reduced, label) do
+      {:op, {op, left, right}} -> {op, nest_ops(reduced, left), nest_ops(reduced, right)}
+      :you -> :you
+    end
+  end
+
+  defp nest_ops(_, label) when is_integer(label) do
+    label
+  end
+
+  defp preloop(map, solved) do
+    pre = pre_solve(map, solved)
+
+    case pre do
+      {^map, solved} -> {map, solved}
+      {new, new_solved} -> preloop(new, new_solved)
+    end
+  end
+
+  defp pre_solve(map, solved) when is_map(map) and is_map(solved) do
+    {new_list, solved} =
+      Enum.map_reduce(map, solved, fn item, solved ->
+        reduce_op(item, solved)
+      end)
+
+    {Map.new(new_list), solved}
+  end
+
+  defp reduce_op({_, {:val, _}} = keep, solved), do: {keep, solved}
+  defp reduce_op({_, :you} = keep, solved), do: {keep, solved}
+  defp reduce_op({"root", {:EQUAL, _, _}} = keep, solved), do: {keep, solved}
+
+  defp reduce_op({k, {:op, {op, left, right}}}, solved) do
+    left =
+      case Map.fetch(solved, left) do
+        {:ok, v} -> v
+        :error -> left
+      end
+
+    right =
+      case Map.fetch(solved, right) do
+        {:ok, v} -> v
+        :error -> right
+      end
+
+    if is_integer(left) and is_integer(right) do
+      value = calc(op, left, right)
+      solved = Map.put(solved, k, value)
+      {{k, {:val, value}}, solved}
+    else
+      {{k, {:op, {op, left, right}}}, solved}
+    end
+  end
+end
---
+++ test/2022/day_21_test.exs
@@ -0,0 +1,77 @@
+defmodule Aoe.Y22.Day21Test do
+  use ExUnit.Case, async: true
+
+  alias Aoe.Y22.Day21, as: Solution, warn: false
+  alias Aoe.Input, warn: false
+
+  # To run the test, run the following command:
+  #
+  #     mix test test/2022/day_21_test.exs
+  #
+  # To run the solution
+  #
+  #     mix aoe.run --year 2022 --day 21 --part 1
+  #
+  # Use sample input:
+  #
+  #     {:ok, path} = Input.resolve(2022, 21, "sample-1")
+  #
+
+  @sample_1 """
+  root: pppw + sjmn
+  dbpl: 5
+  cczh: sllz + lgvd
+  zczc: 2
+  ptdq: humn - dvpt
+  dvpt: 3
+  lfqf: 4
+  humn: 5
+  ljgn: 2
+  sjmn: drzm * dbpl
+  sllz: 4
+  pppw: cczh / lfqf
+  lgvd: ljgn * ptdq
+  drzm: hmdt - zczc
+  hmdt: 32
+  """
+
+  test "verify 2022/21 part_one - samples" do
+    problem =
+      @sample_1
+      |> Input.as_file()
+      |> Solution.read_file!(:part_one)
+      |> Solution.parse_input!(:part_one)
+
+    expected = 152
+    assert expected == Solution.part_one(problem)
+  end
+
+  test "verify 2022/21 part_two - samples" do
+    problem =
+      @sample_1
+      |> Input.as_file()
+      |> Solution.read_file!(:part_two)
+      |> Solution.parse_input!(:part_two)
+
+    expected = 301
+    assert expected == Solution.part_two(problem)
+  end
+
+  # Once your part one was successfully sumbitted, you may uncomment this test
+  # to ensure your implementation was not altered when you implement part two.
+
+  # @part_one_solution CHANGE_ME
+  #
+  # test "verify 2022/21 part one" do
+  #   assert {:ok, @part_one_solution} == Aoe.run(2022, 21, :part_one)
+  # end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  # @part_two_solution CHANGE_ME
+  #
+  # test "verify 2022/21 part two" do
+  #   assert {:ok, @part_two_solution} == Aoe.run(2022, 21, :part_two)
+  # end
+end
