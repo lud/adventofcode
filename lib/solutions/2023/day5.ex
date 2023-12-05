44b2920e3a12eb375d22fa5e82ae87a12f6345ac
lud-wj <ludovic@demblans.com>
2023-12-05 08:03:22 +0100

    p1 ok

---
+++ lib/solutions/2023/day5.ex
@@ -0,0 +1,89 @@
+defmodule AdventOfCode.Y23.Day5 do
+  alias AoC.Input, warn: false
+
+  def read_file(file, _part) do
+    Input.read!(file)
+  end
+
+  def parse_input(input, _part) do
+    blocks = input |> String.trim_trailing() |> String.split("\n\n")
+
+    [
+      "seeds: " <> seeds_raw,
+      "seed-to-soil map:\n" <> seed_to_soil_raw,
+      "soil-to-fertilizer map:\n" <> soil_to_fertilizer_raw,
+      "fertilizer-to-water map:\n" <> fertilizer_to_water_raw,
+      "water-to-light map:\n" <> water_to_light_raw,
+      "light-to-temperature map:\n" <> light_to_temperature_raw,
+      "temperature-to-humidity map:\n" <> temperature_to_humidity_raw,
+      "humidity-to-location map:\n" <> humidity_to_location_raw
+    ] =
+      blocks
+      |> dbg()
+
+    %{
+      seeds: int_list(seeds_raw),
+      seed_to_soil: parse_ranges(seed_to_soil_raw),
+      soil_to_fertilizer: parse_ranges(soil_to_fertilizer_raw),
+      fertilizer_to_water: parse_ranges(fertilizer_to_water_raw),
+      water_to_light: parse_ranges(water_to_light_raw),
+      light_to_temperature: parse_ranges(light_to_temperature_raw),
+      temperature_to_humidity: parse_ranges(temperature_to_humidity_raw),
+      humidity_to_location: parse_ranges(humidity_to_location_raw)
+    }
+  end
+
+  defp int_list(line) do
+    line |> String.split(" ") |> Enum.map(&String.to_integer/1)
+  end
+
+  defp parse_ranges(lines) do
+    lines
+    |> String.split("\n")
+    |> Enum.map(&parse_range/1)
+    |> dbg(charlists: :as_lists)
+  end
+
+  defp parse_range(line) do
+    line |> dbg()
+    [dest_0, source_0, len] = int_list(line)
+
+    source_range = source_0..(source_0 + len - 1)//1
+    dest_range = dest_0..(dest_0 + len - 1)//1
+    {source_range, dest_range}
+  end
+
+  def part_one(problem) do
+    locations = Enum.map(problem.seeds, &find_location(&1, problem))
+    Enum.min(locations)
+  end
+
+  defp find_location(seed, data) do
+    path = [
+      :seed_to_soil,
+      :soil_to_fertilizer,
+      :fertilizer_to_water,
+      :water_to_light,
+      :light_to_temperature,
+      :temperature_to_humidity,
+      :humidity_to_location
+    ]
+
+    Enum.reduce(path, seed, fn tl_key, id -> translate(Map.fetch!(data, tl_key), id) end)
+  end
+
+  defp translate(ranges, id) do
+    case Enum.find(ranges, fn {source_range, _dest_range} -> id in source_range end) do
+      {source_range, dest_range} ->
+        diff = id - source_range.first
+        dest = dest_range.first + diff
+
+      nil ->
+        id
+    end
+  end
+
+  # def part_two(problem) do
+  #   problem
+  # end
+end
---
+++ test/2023/day5_test.exs
@@ -0,0 +1,101 @@
+defmodule AdventOfCode.Y23.Day5Test do
+  alias AoC.Input, warn: false
+  alias AdventOfCode.Y23.Day5, as: Solution, warn: false
+  use ExUnit.Case, async: true
+
+  # To run the test, run one of the following commands:
+  #
+  #     mix AoC.test --year 2023 --day 5
+  #
+  #     mix test test/2023/day5_test.exs
+  #
+  # To run the solution
+  #
+  #     mix AoC.run --year 2023 --day 5 --part 1
+  #
+  # Use sample input file, for instance in priv/input/2023/"day-5-sample.inp"
+  #
+  #     {:ok, path} = Input.resolve(2023, 5, "sample")
+  #
+  # Good luck!
+
+  defp solve(input, part) do
+    problem =
+      input
+      |> Input.as_file()
+      |> Solution.read_file(part)
+      |> Solution.parse_input(part)
+
+    apply(Solution, part, [problem])
+  end
+
+  test "part one example" do
+    input = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+    """
+
+    assert 35 == solve(input, :part_one)
+  end
+
+  # test "part two example" do
+  #   input = """
+  #   This is an
+  #   example input.
+  #   replace with
+  #   an example from
+  #   the AoC website.
+  #   """
+  #
+  #   assert CHANGE_ME == solve(input, :part_two)
+  # end
+
+  # Once your part-one was successfully sumbitted, you may uncomment this test
+  # to ensure your implementation was not altered when you implement part two.
+
+  # @part_one_solution CHANGE_ME
+  #
+  # test "part one solution" do
+  #   assert {:ok, @part_one_solution} == AoC.run(2023, 5, :part_one)
+  # end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  # @part_two_solution CHANGE_ME
+  #
+  # test "part two solution" do
+  #   assert {:ok, @part_two_solution} == AoC.run(2023, 5, :part_two)
+  # end
+end
