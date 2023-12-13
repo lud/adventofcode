df37bc62c2a13f60521a5ca81a9404c532a74ce3
lud-wj <ludovic@demblans.com>
2023-12-13 08:52:07 +0100

    almost

--- lib/solutions/2023/day13.ex
+++ lib/solutions/2023/day13.ex
@@ -14,25 +14,66 @@ defmodule AdventOfCode.Y23.Day13 do
     |> Enum.with_index(1)
     |> Enum.map(fn {pattern, n} ->
       IO.puts(" == Pattern #{n} ==")
-      compute_value(pattern)
+      {:ok, v} = compute_value(pattern)
+      v
     end)
     |> Enum.sum()
   end
 
+  def part_two(problem) do
+    problem
+    |> Enum.map(&compute_new_count/1)
+    |> Enum.sum()
+  end
+
+  defp compute_new_count(pattern) do
+    {:ok, old_value} = compute_value(pattern)
+    old_value |> IO.inspect(label: ~S/old_value/)
+
+    pattern
+    |> stream_changes()
+    |> Enum.find_value(fn new_pattern ->
+      case compute_value(new_pattern) do
+        :error -> nil
+        {:ok, ^old_value} -> nil
+        {:ok, new_value} -> new_value
+      end
+    end)
+  end
+
+  defp stream_changes(pattern) do
+    max_y = length(pattern) - 1
+    max_x = (pattern |> hd() |> String.length()) - 1
+    binding() |> IO.inspect(label: ~S/binding()/)
+
+    Stream.resource(
+      fn -> {0, 0} end,
+      fn {x, y} ->
+        y |> IO.inspect(label: ~S/y/)
+        x |> IO.inspect(label: ~S/x/)
+        {[fix_pattern(pattern, x, y)], {x + 1, y}}
+      end,
+      fn _ -> nil end
+    )
+  end
+
+  defp fix_pattern(pattern, x, y) do
+    List.update_at(pattern, y, fn <<prev::binary-size(x), c, rest::binary>> ->
+      new_c =
+        case c do
+          ?. -> ?#
+          ?# -> ?.
+        end
+
+      <<prev::binary-size(x), new_c, rest::binary>>
+    end)
+    |> dbg()
+  end
+
   defp compute_value(pattern) do
     case h_reflection(pattern) do
-      {:ok, v} ->
-        v * 100
-
-      :error ->
-        case v_reflection(pattern) do
-          {:ok, v} ->
-            v
-
-          :error ->
-            print_pattern(pattern)
-            raise "bad pattern"
-        end
+      {:ok, v} -> {:ok, v * 100}
+      :error -> v_reflection(pattern)
     end
   end
 
@@ -104,8 +145,4 @@ defmodule AdventOfCode.Y23.Day13 do
     Enum.intersperse(pattern, "\n") |> Enum.join() |> IO.puts()
     pattern
   end
-
-  # def part_two(problem) do
-  #   problem
-  # end
 end
--- test/2023/day13_test.exs
+++ test/2023/day13_test.exs
@@ -86,26 +86,39 @@ defmodule AdventOfCode.Y23.Day13Test do
     assert solve(input, :part_one)
   end
 
-  # Once your part one was successfully sumbitted, you may uncomment this test
-  # to ensure your implementation was not altered when you implement part two.
+  @part_one_solution 34889
 
-  # @part_one_solution CHANGE_ME
-  #
-  # test "part one solution" do
-  #   assert {:ok, @part_one_solution} == AoC.run(2023, 13, :part_one)
-  # end
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2023, 13, :part_one)
+  end
 
-  # test "part two example" do
-  #   input = """
-  #   This is an
-  #   example input.
-  #   replace with
-  #   an example from
-  #   the AoC website.
-  #   """
-  #
-  #   assert CHANGE_ME == solve(input, :part_two)
-  # end
+  test "part two example" do
+    input = """
+    #.##..##.
+    ..#.##.#.
+    ##......#
+    ##......#
+    ..#.##.#.
+    ..##..##.
+    #.#.##.#.
+    """
+
+    assert 300 == solve(input, :part_two)
+  end
+
+  test "part two example 2" do
+    input = """
+    #...##..#
+    #....#..#
+    ..##..###
+    #####.##.
+    #####.##.
+    ..##..###
+    #....#..#
+    """
+
+    assert 100 == solve(input, :part_two)
+  end
 
   # You may also implement a test to validate the part two to ensure that you
   # did not broke your shared modules when implementing another problem.
