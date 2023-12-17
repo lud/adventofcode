c7883a0f1e3fd47d32ff1a31194d5ebd784c64fb
lud-wj <ludovic@demblans.com>
2023-12-17 17:58:59 +0100

    p1 ok

---
+++ lib/solutions/2023/day17.ex
@@ -0,0 +1,126 @@
+defmodule AdventOfCode.Y23.Day17 do
+  alias AoC.Input, warn: false
+  alias AoC.Grid, warn: false
+
+  def read_file(file, _part) do
+    Input.stream!(file, trim: true)
+  end
+
+  def parse_input(input, _part) do
+    input |> Grid.parse_stream(fn x -> {:ok, String.to_integer(x)} end)
+  end
+
+  def part_one(grid) do
+    target_xy = {Grid.max_x(grid), Grid.max_y(grid)} |> dbg()
+
+    start_poses = [
+      {{0, 0}, {:e, 0}, 0},
+      {{0, 0}, {:s, 0}, 0}
+    ]
+
+    find_target(start_poses, target_xy, %{}, grid)
+  end
+
+  defp find_target([{target_xy, _, cost} | open], target_xy, seen, grid) do
+    cost
+  end
+
+  defp find_target([h | open], target_xy, seen, grid) do
+    {next_poses, seen} =
+      next_poses(h, grid)
+      |> Enum.flat_map_reduce(seen, fn {xy, dc, _} = node, seen ->
+        key = {xy, dc}
+
+        if Map.has_key?(seen, key) do
+          {[], seen}
+        else
+          {[node], Map.put(seen, key, true)}
+        end
+      end)
+
+    open = insert_all(next_poses, open) |> Enum.take(1000)
+    find_target(open, target_xy, seen, grid)
+  end
+
+  defp insert_all([h | t], open) do
+    insert_all(t, insert(h, open))
+  end
+
+  defp insert_all([], open) do
+    open
+  end
+
+  defp insert({xy, dc, cost} = new_best, [{_xy, _dc, best} = cand | open]) when cost <= best do
+    [new_best, cand | open]
+  end
+
+  defp insert(new, [best | open]) do
+    [best | insert(new, open)]
+  end
+
+  defp insert(new, []) do
+    [new]
+  end
+
+  defp next_poses({xy, {dir, count}, cost}, grid) when count < 3 do
+    xy_cont = Grid.translate(xy, dir)
+
+    poses =
+      case Map.fetch(grid, xy_cont) do
+        {:ok, add_cost} -> [{xy_cont, {dir, count + 1}, cost + add_cost}]
+        :error -> []
+      end
+
+    left_dir = turn_left(dir)
+    right_dir = turn_right(dir)
+    left_xy = Grid.translate(xy, left_dir)
+    right_xy = Grid.translate(xy, right_dir)
+
+    poses =
+      case Map.fetch(grid, left_xy) do
+        {:ok, add_cost} -> [{left_xy, {left_dir, 1}, cost + add_cost} | poses]
+        :error -> poses
+      end
+
+    poses =
+      case Map.fetch(grid, right_xy) do
+        {:ok, add_cost} -> [{right_xy, {right_dir, 1}, cost + add_cost} | poses]
+        :error -> poses
+      end
+
+    poses
+  end
+
+  defp next_poses({xy, {dir, 3}, cost}, grid) do
+    poses = []
+
+    left_dir = turn_left(dir)
+    right_dir = turn_right(dir)
+    left_xy = Grid.translate(xy, left_dir)
+    right_xy = Grid.translate(xy, right_dir)
+
+    poses =
+      case Map.fetch(grid, left_xy) do
+        {:ok, add_cost} -> [{left_xy, {left_dir, 1}, cost + add_cost} | poses]
+        :error -> poses
+      end
+
+    poses =
+      case Map.fetch(grid, right_xy) do
+        {:ok, add_cost} -> [{right_xy, {right_dir, 1}, cost + add_cost} | poses]
+        :error -> poses
+      end
+
+    poses
+  end
+
+  defp turn_left(:e), do: :n
+  defp turn_left(:n), do: :w
+  defp turn_left(:w), do: :s
+  defp turn_left(:s), do: :e
+
+  defp turn_right(:e), do: :s
+  defp turn_right(:s), do: :w
+  defp turn_right(:w), do: :n
+  defp turn_right(:n), do: :e
+end
---
+++ test/2023/day17_test.exs
@@ -0,0 +1,81 @@
+defmodule AdventOfCode.Y23.Day17Test do
+  alias AoC.Input, warn: false
+  alias AdventOfCode.Y23.Day17, as: Solution, warn: false
+  use ExUnit.Case, async: true
+
+  # To run the test, run one of the following commands:
+  #
+  #     mix AoC.test --year 2023 --day 17
+  #
+  #     mix test test/2023/day17_test.exs
+  #
+  # To run the solution
+  #
+  #     mix AoC.run --year 2023 --day 17 --part 1
+  #
+  # Use sample input file, for instance in priv/input/2023/"day-17-sample.inp"
+  #
+  #     {:ok, path} = Input.resolve(2023, 17, "sample")
+  #
+  # Good luck!
+
+  defp solve(input, part) do
+    problem =
+      input
+      |> Input.as_file()
+      |> Solution.read_file(part)
+      |> Solution.parse_input(part)
+
+    apply(Solution, part, [problem])
+  end
+
+  test "part one example" do
+    input = ~S"""
+    2413432311323
+    3215453535623
+    3255245654254
+    3446585845452
+    4546657867536
+    1438598798454
+    4457876987766
+    3637877979653
+    4654967986887
+    4564679986453
+    1224686865563
+    2546548887735
+    4322674655533
+    """
+
+    assert 102 == solve(input, :part_one)
+  end
+
+  # Once your part one was successfully sumbitted, you may uncomment this test
+  # to ensure your implementation was not altered when you implement part two.
+
+  # @part_one_solution CHANGE_ME
+  #
+  # test "part one solution" do
+  #   assert {:ok, @part_one_solution} == AoC.run(2023, 17, :part_one)
+  # end
+
+  # test "part two example" do
+  #   input = ~S"""
+  #   This is an
+  #   example input.
+  #   replace with
+  #   an example from
+  #   the AoC website.
+  #   """
+  #
+  #   assert CHANGE_ME == solve(input, :part_two)
+  # end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  # @part_two_solution CHANGE_ME
+  #
+  # test "part two solution" do
+  #   assert {:ok, @part_two_solution} == AoC.run(2023, 17, :part_two)
+  # end
+end
