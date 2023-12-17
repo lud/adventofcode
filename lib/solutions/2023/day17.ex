2d66289e92599b669e84aff271f3a3da08024f6c
lud-wj <ludovic@demblans.com>
2023-12-17 18:23:03 +0100

    slow

--- lib/solutions/2023/day17.ex
+++ lib/solutions/2023/day17.ex
@@ -32,7 +32,7 @@ defmodule AdventOfCode.Y23.Day17 do
     find_target(start_poses, target_xy, %{}, grid, :part_two)
   end
 
-  defp find_target([{target_xy, _, cost} | open], target_xy, seen, grid, _) do
+  defp find_target([{target_xy, _, cost} | _], target_xy, _, _, _) do
     cost
   end
 
@@ -49,7 +49,7 @@ defmodule AdventOfCode.Y23.Day17 do
         end
       end)
 
-    open = insert_all(next_poses, open)
+    open = insert_all(next_poses, open) |> Enum.take(2000)
     find_target(open, target_xy, seen, grid, part)
   end
 
@@ -61,8 +61,12 @@ defmodule AdventOfCode.Y23.Day17 do
     open
   end
 
-  defp insert({xy, dc, cost} = new_best, [{_xy, _dc, best} = cand | open]) when cost <= best do
+  defp insert({_, _, cost} = new_best, [{_xy, _dc, best} = cand | open]) when cost <= best do
     [new_best, cand | open]
+  end
+
+  defp insert(same, [same | open]) do
+    [same | open]
   end
 
   defp insert(new, [best | open]) do
@@ -76,8 +80,8 @@ defmodule AdventOfCode.Y23.Day17 do
   # -- Next positions for part two --------------------------------------------
 
   defp next_poses({xy, {dir, count}, cost}, grid, :part_two) do
-    can_continue? = count < 10
-    can_turn? = count > 4
+    can_continue? = count <= 9
+    can_turn? = count >= 4
 
     poses =
       if can_continue? do
@@ -117,37 +121,20 @@ defmodule AdventOfCode.Y23.Day17 do
 
   # -- Next positions for part one --------------------------------------------
 
-  defp next_poses({xy, {dir, count}, cost}, grid, :part_one) when count < 3 do
-    xy_cont = Grid.translate(xy, dir)
+  defp next_poses({xy, {dir, count}, cost}, grid, :part_one) do
+    can_continue? = count < 3
 
     poses =
-      case Map.fetch(grid, xy_cont) do
-        {:ok, add_cost} -> [{xy_cont, {dir, count + 1}, cost + add_cost}]
-        :error -> []
+      if can_continue? do
+        xy_cont = Grid.translate(xy, dir)
+
+        case Map.fetch(grid, xy_cont) do
+          {:ok, add_cost} -> [{xy_cont, {dir, count + 1}, cost + add_cost}]
+          :error -> []
+        end
+      else
+        []
       end
-
-    left_dir = turn_left(dir)
-    right_dir = turn_right(dir)
-    left_xy = Grid.translate(xy, left_dir)
-    right_xy = Grid.translate(xy, right_dir)
-
-    poses =
-      case Map.fetch(grid, left_xy) do
-        {:ok, add_cost} -> [{left_xy, {left_dir, 1}, cost + add_cost} | poses]
-        :error -> poses
-      end
-
-    poses =
-      case Map.fetch(grid, right_xy) do
-        {:ok, add_cost} -> [{right_xy, {right_dir, 1}, cost + add_cost} | poses]
-        :error -> poses
-      end
-
-    poses
-  end
-
-  defp next_poses({xy, {dir, 3}, cost}, grid, :part_one) do
-    poses = []
 
     left_dir = turn_left(dir)
     right_dir = turn_right(dir)
--- test/2023/day17_test.exs
+++ test/2023/day17_test.exs
@@ -49,9 +49,6 @@ defmodule AdventOfCode.Y23.Day17Test do
     assert 102 == solve(input, :part_one)
   end
 
-  # Once your part one was successfully sumbitted, you may uncomment this test
-  # to ensure your implementation was not altered when you implement part two.
-
   @part_one_solution 1195
 
   test "part one solution" do
@@ -78,12 +75,9 @@ defmodule AdventOfCode.Y23.Day17Test do
     assert 94 == solve(input, :part_two)
   end
 
-  # You may also implement a test to validate the part two to ensure that you
-  # did not broke your shared modules when implementing another problem.
+  @part_two_solution 1347
 
-  # @part_two_solution CHANGE_ME
-  #
-  # test "part two solution" do
-  #   assert {:ok, @part_two_solution} == AoC.run(2023, 17, :part_two)
-  # end
+  test "part two solution" do
+    assert {:ok, @part_two_solution} == AoC.run(2023, 17, :part_two)
+  end
 end
