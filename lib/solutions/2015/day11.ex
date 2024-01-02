7c5c159ea9c77f010e53d9a5c55589a21af7bd9b
lud-wj <ludovic@demblans.com>
2024-01-02 16:18:15 +0100

    p1 ok

---
+++ lib/solutions/2015/day11.ex
@@ -0,0 +1,94 @@
+defmodule AdventOfCode.Y15.Day11 do
+  alias AoC.Input
+
+  def read_file(file, _part) do
+    Input.read!(file) |> String.trim()
+  end
+
+  def parse_input(input, _part) do
+    input |> String.to_charlist()
+  end
+
+  def part_one(problem) do
+    problem
+    |> increment()
+    |> Stream.iterate(&increment/1)
+
+    # |> Stream.map(&IO.inspect(&1, label: "??"))
+    |> Stream.filter(&valid?/1)
+    # |> Stream.take(100)
+    # |> Enum.each(&IO.inspect(&1, label: "======="))
+    |> Enum.take(1)
+    |> to_string()
+  end
+
+  defp valid?(chars) do
+    not invalid_chars?(chars) &&
+      has_straight?(chars) &&
+      has_two_pairs?(chars)
+  end
+
+  defp invalid_chars?([?i | _]), do: true
+  defp invalid_chars?([?o | _]), do: true
+  defp invalid_chars?([?l | _]), do: true
+  defp invalid_chars?([_ | t]), do: invalid_chars?(t)
+  defp invalid_chars?([]), do: false
+
+  defp has_straight?([a, b, c | _]) when a + 1 == b and b + 1 == c, do: true
+  defp has_straight?([_, _]), do: false
+  defp has_straight?([_ | t]), do: has_straight?(t)
+
+  defp has_two_pairs?(chars) do
+    has_two_pairs?(chars, nil)
+  end
+
+  defp has_two_pairs?([a, a | t], nil) do
+    has_two_pairs?(t, a)
+  end
+
+  defp has_two_pairs?([a, a | _], prev) when a != prev do
+    true
+  end
+
+  defp has_two_pairs?([_, a | t], prev) do
+    has_two_pairs?([a | t], prev)
+  end
+
+  defp has_two_pairs?([_], _) do
+    false
+  end
+
+  defp has_two_pairs?([], _) do
+    false
+  end
+
+  defp increment(chars) do
+    {:ok, next} = do_increment(chars)
+    next
+  end
+
+  defp do_increment([?z]) do
+    {:up, [?a]}
+  end
+
+  defp do_increment([c]) do
+    {:ok, [c + 1]}
+  end
+
+  defp do_increment([c | t]) do
+    case do_increment(t) do
+      {:ok, new_t} ->
+        {:ok, [c | new_t]}
+
+      {:up, new_t} ->
+        case c do
+          ?z -> {:up, [?a | new_t]}
+          c -> {:ok, [c + 1 | new_t]}
+        end
+    end
+  end
+
+  # def part_two(problem) do
+  #   problem
+  # end
+end
---
+++ test/2015/day11_test.exs
@@ -0,0 +1,66 @@
+defmodule AdventOfCode.Y15.Day11Test do
+  alias AoC.Input, warn: false
+  alias AdventOfCode.Y15.Day11, as: Solution, warn: false
+  use ExUnit.Case, async: true
+
+  # To run the test, run one of the following commands:
+  #
+  #     mix AoC.test --year 2015 --day 11
+  #
+  #     mix test test/2015/day11_test.exs
+  #
+  # To run the solution
+  #
+  #     mix AoC.run --year 2015 --day 11 --part 1
+  #
+  # Use sample input file, for instance in priv/input/2015/"day-11-sample.inp"
+  #
+  #     {:ok, path} = Input.resolve(2015, 11, "sample")
+  #
+  # Good luck!
+
+  defp solve(input, part) do
+    problem =
+      input
+      |> Input.as_file()
+      |> Solution.read_file(part)
+      |> Solution.parse_input(part)
+
+    apply(Solution, part, [problem])
+  end
+
+  test "part one example" do
+    input = ~S"""
+    abcdefgh
+    """
+
+    assert "abcdffaa" == solve(input, :part_one)
+  end
+
+  @part_one_solution "hxbxxyzz"
+
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2015, 11, :part_one)
+  end
+
+  # test "part two example" do
+  #   input = ~S"""
+  #   This is an
+  #   example input.
+  #   replace with
+  #   an example from
+  #   the AoC website.
+  #   """
+  #
+  #   assert CHANGE_ME == solve(input, :part_two)
+  # end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  # @part_two_solution CHANGE_ME
+  #
+  # test "part two solution" do
+  #   assert {:ok, @part_two_solution} == AoC.run(2015, 11, :part_two)
+  # end
+end
