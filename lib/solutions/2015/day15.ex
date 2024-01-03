99118fbdd18554fa8a812043d14c71abdcd42e88
lud-wj <ludovic@demblans.com>
2024-01-03 01:48:28 +0100

    refactor

--- lib/solutions/2015/day15.ex
+++ lib/solutions/2015/day15.ex
@@ -29,16 +29,21 @@ defmodule AdventOfCode.Y15.Day15 do
   end
 
   def part_one(ingredients) do
+    # In part one we add each teaspoon one by one, trying all possible
+    # ingredients and keeping the composition that yields the best score.  This
+    # is much more efficient than generating all possible permutations of the
+    # ingredients as in part two.
+
     composition = Map.new(ingredients, fn {name, _} -> {name, 0} end) |> Map.put(:score, 0)
 
-    # force each ingredient to be used at least once because of the 0 values
-
+    # force each ingredient to be used at least once, otherwise their zero
+    # values will make all scores to be zero, since we add each teaspoon one by
+    # one.
     iterations = 100 - map_size(ingredients)
-
     composition = Map.new(composition, fn {name, _} -> {name, 1} end)
 
     best =
-      Enum.reduce(1..iterations, composition, fn i, best_comp ->
+      Enum.reduce(1..iterations, composition, fn _i, best_comp ->
         ingredients
         |> Enum.map(fn {name, _} -> add_comp(best_comp, name, ingredients) end)
         |> Enum.max_by(& &1.score)
@@ -92,30 +97,51 @@ defmodule AdventOfCode.Y15.Day15 do
   end
 
   def part_two(ingredients) do
+    # try to cook all possible cookies
+
+    # we generate all permutions for N ingredients that add up to 100
+
+    # we flat_map for the number of ingredients minus 2
     [_, _ | flatmap_count] = ingrnames = Map.keys(ingredients)
+
+    # the first ingredient initialises the teaspoons permutation with a number
+    # between 0 and 100, in a list
     stream = Stream.map(100..0, &[&1])
 
+    # then for each ingredient in the middle, we flat map the stream with 0 to
+    # N-1 possible remaining teaspoons. This does not happen in the test example
+    # as there are only two ingredients.
     stream =
       Enum.reduce(flatmap_count, stream, fn _, stream ->
         Stream.flat_map(stream, &permut_teaspoons/1)
       end)
 
+    # for the last ingredient, we just add the remaining teaspoons so the sum is
+    # 100
     stream = Stream.map(stream, &last_permut/1)
 
-    stream = Stream.map(stream, fn tsps -> Enum.zip(ingrnames, tsps) end)
+    # The we zip to get the composition and we cook the cookie. This could be in
+    # the same Stream.map as the previous one but for clarity I separated them.
+    # It does not change very much the performance.
+    stream =
+      Stream.map(stream, fn all_portions ->
+        composition = Enum.zip(ingrnames, all_portions)
+        cook(composition, ingredients)
+      end)
 
-    stream = Stream.map(stream, fn composition -> cook(composition, ingredients) end)
-
+    # Finally we filter the cookies that have 500 calories and we turn that into
+    # scores.
     stream = Stream.filter(stream, fn cookie -> cookie.calories == 500 end)
-
     stream = Stream.map(stream, &cookie_score/1)
-    stream |> Enum.max()
+
+    # And we return the maximum score
+    Enum.max(stream)
   end
 
   defp permut_teaspoons(used) do
     case Enum.sum(used) do
       100 -> [[0 | used]]
-      n -> Stream.map(1..(100 - n), fn i -> [i | used] end)
+      n -> Stream.map(0..(100 - n), fn i -> [i | used] end)
     end
   end
 
