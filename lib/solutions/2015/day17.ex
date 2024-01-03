ba20bc5b88e58ead69dbcc7e2d6ecea260d073f1
lud-wj <ludovic@demblans.com>
2024-01-03 12:41:42 +0100

    d17 ok

---
+++ lib/solutions/2015/day17.ex
@@ -0,0 +1,50 @@
+defmodule AdventOfCode.Y15.Day17 do
+  alias AoC.Input
+
+  def read_file(file, _part) do
+    Input.stream!(file, trim: true)
+  end
+
+  def parse_input(input, _part) do
+    input |> Enum.map(&String.to_integer/1)
+  end
+
+  def part_one(problem, liters \\ 150) do
+    # We have N containers. We will generate permutations of those containers,
+    # using bits to tell which container is used.
+    max_int = trunc(:math.pow(2, length(problem))) - 1
+
+    1..max_int
+    |> Stream.map(&match_containers(&1, problem))
+    |> Enum.count(fn {contains, _} -> contains == liters end)
+  end
+
+  def part_two(problem, liters \\ 150) do
+    max_int = trunc(:math.pow(2, length(problem))) - 1
+
+    1..max_int
+    |> Stream.map(&match_containers(&1, problem))
+    |> Enum.reduce({99999, 0}, fn
+      {^liters, same}, {same, n_best} -> {same, n_best + 1}
+      {^liters, lower}, {best, _} when lower < best -> {lower, 1}
+      _, acc -> acc
+    end)
+    |> elem(1)
+  end
+
+  defp match_containers(int, containers) do
+    match_containers(int, containers, 1, 0, 0)
+  end
+
+  defp match_containers(n, [h | t], pw, sum, count) when Bitwise.band(n, pw) == pw do
+    match_containers(n, t, pw * 2, sum + h, count + 1)
+  end
+
+  defp match_containers(n, [_ | t], pw, sum, count) do
+    match_containers(n, t, pw * 2, sum, count)
+  end
+
+  defp match_containers(_, [], _, sum, count) do
+    {sum, count}
+  end
+end
---
+++ test/2015/day17_test.exs
@@ -0,0 +1,67 @@
+defmodule AdventOfCode.Y15.Day17Test do
+  alias AoC.Input, warn: false
+  alias AdventOfCode.Y15.Day17, as: Solution, warn: false
+  use ExUnit.Case, async: true
+
+  # To run the test, run one of the following commands:
+  #
+  #     mix AoC.test --year 2015 --day 17
+  #
+  #     mix test test/2015/day17_test.exs
+  #
+  # To run the solution
+  #
+  #     mix AoC.run --year 2015 --day 17 --part 1
+  #
+  # Use sample input file, for instance in priv/input/2015/"day-17-sample.inp"
+  #
+  #     {:ok, path} = Input.resolve(2015, 17, "sample")
+  #
+  # Good luck!
+
+  defp solve(input, part, liters) do
+    problem =
+      input
+      |> Input.as_file()
+      |> Solution.read_file(part)
+      |> Solution.parse_input(part)
+
+    apply(Solution, part, [problem, liters])
+  end
+
+  test "part one example" do
+    input = ~S"""
+    20
+    15
+    10
+    5
+    5
+    """
+
+    assert 4 == solve(input, :part_one, 25)
+  end
+
+  @part_one_solution 654
+
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2015, 17, :part_one)
+  end
+
+  test "part two example" do
+    input = ~S"""
+    20
+    15
+    10
+    5
+    5
+    """
+
+    assert 3 == solve(input, :part_two, 25)
+  end
+
+  @part_two_solution 57
+
+  test "part two solution" do
+    assert {:ok, @part_two_solution} == AoC.run(2015, 17, :part_two)
+  end
+end
