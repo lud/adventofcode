08589140ed904fc25d050de36609ae817ae18e0e
lud-wj <ludovic@demblans.com>
2024-01-08 14:56:11 +0100

    p1 ok

---
+++ lib/solutions/2015/day19.ex
@@ -0,0 +1,71 @@
+defmodule AdventOfCode.Y15.Day19 do
+  alias AoC.Input
+
+  def read_file(file, _part) do
+    Input.read!(file)
+  end
+
+  def parse_input(input, _part) do
+    [repls, start_molecule] = input |> String.trim() |> String.split("\n\n")
+    repls = repls |> String.split("\n") |> Enum.map(&parse_replacement/1) |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
+    {repls, start_molecule}
+  end
+
+  defp parse_replacement(repl) do
+    [from, to] = String.split(repl, " => ")
+    {from, to}
+  end
+
+  def part_one({replacements, molecule}) do
+    binding() |> IO.inspect(label: ~S/binding()/)
+    parts = split_molecule(molecule, replacements, [])
+
+    reduce(parts, [], replacements, %{}) |> map_size() |> dbg()
+  end
+
+  defp reduce([h | t], prefix, replacements, results) when is_map_key(replacements, h) do
+    results =
+      replacements
+      |> Map.fetch!(h)
+      |> Enum.reduce(results, fn to, acc ->
+        new_molecule = to_string([prefix, to, t])
+        Map.put(acc, new_molecule, true)
+      end)
+
+    reduce(t, prefix ++ [h], replacements, results)
+  end
+
+  defp reduce([h | t], prefix, replacements, results) do
+    reduce(t, prefix ++ [h], replacements, results)
+  end
+
+  defp reduce([], _, _, results) do
+    results
+  end
+
+  defp split_molecule(<<>>, replacements, parts) do
+    :lists.reverse(parts)
+  end
+
+  defp split_molecule(molecule, replacements, parts) do
+    found =
+      Enum.find(replacements, fn {from, tos} ->
+        binding() |> IO.inspect(label: ~S/binding()/)
+        String.starts_with?(molecule, from)
+      end)
+
+    case found do
+      {from, _} ->
+        {^from, molecule} = String.split_at(molecule, String.length(from))
+        split_molecule(molecule, replacements, [from | parts])
+
+      nil ->
+        {letter, molecule} = String.split_at(molecule, 1)
+        split_molecule(molecule, replacements, [letter | parts])
+    end
+  end
+
+  # def part_two(problem) do
+  #   problem
+  # end
+end
---
+++ test/2015/day19_test.exs
@@ -0,0 +1,70 @@
+defmodule AdventOfCode.Y15.Day19Test do
+  alias AoC.Input, warn: false
+  alias AdventOfCode.Y15.Day19, as: Solution, warn: false
+  use ExUnit.Case, async: true
+
+  # To run the test, run one of the following commands:
+  #
+  #     mix AoC.test --year 2015 --day 19
+  #
+  #     mix test test/2015/day19_test.exs
+  #
+  # To run the solution
+  #
+  #     mix AoC.run --year 2015 --day 19 --part 1
+  #
+  # Use sample input file, for instance in priv/input/2015/"day-19-sample.inp"
+  #
+  #     {:ok, path} = Input.resolve(2015, 19, "sample")
+  #
+  # Good luck!
+
+  defp solve(input, part) do
+    problem =
+      input
+      |> Input.as_file()
+      |> Solution.read_file(part)
+      |> Solution.parse_input(part)
+
+    apply(Solution, part, [problem])
+  end
+
+  test "part one example" do
+    input = ~S"""
+    H => HO
+    H => OH
+    O => HH
+
+    HOH
+    """
+
+    assert 4 == solve(input, :part_one)
+  end
+
+  @part_one_solution 535
+
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2015, 19, :part_one)
+  end
+
+  # test "part two example" do
+  #   input = ~S"""
+  #   This is an
+  #   example input.
+  #   replace with
+  #   an example from
+  #   the AoC website.
+  #   """
+  #
+  #   assert CHANGE_ME == solve(input, :part_two)
+  # end
+
+  # You may also implement a test to validate the part two to ensure that you
+  # did not broke your shared modules when implementing another problem.
+
+  # @part_two_solution CHANGE_ME
+  #
+  # test "part two solution" do
+  #   assert {:ok, @part_two_solution} == AoC.run(2015, 19, :part_two)
+  # end
+end
