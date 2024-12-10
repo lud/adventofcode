a2a4fba6ddea4ebf8cc2ac4c16627fe8c05c3738
lud-wj <ludovic@demblans.com>
2024-12-10 09:17:15 +0100

    d10 ok

--- lib/solutions/2024/day10.ex
+++ lib/solutions/2024/day10.ex
@@ -3,24 +3,18 @@ defmodule AdventOfCode.Solutions.Y24.Day10 do
   alias AoC.Input
 
   def parse(input, _part) do
-    input |> Input.stream!() |> Grid.parse_lines(fn c -> {:ok, c - ?0} end) |> elem(0)
+    input
+    |> Input.stream!()
+    |> Grid.parse_lines(fn
+      ?. -> :ignore
+      c -> {:ok, c - ?0}
+    end)
+    |> elem(0)
   end
 
   def part_one(grid) do
-    heads =
-      Enum.flat_map(grid, fn
-        {k, 0} -> [k]
-        _ -> []
-      end)
-
-    summits =
-      Enum.flat_map(grid, fn
-        {k, 9} -> [k]
-        _ -> []
-      end)
-
     scores =
-      for h <- heads, s <- summits, reduce: %{} do
+      for h <- heads(grid), s <- summits(grid), reduce: %{} do
         scores ->
           case Grid.bfs_path(grid, h, s, &uphill_neighbors/2) do
             {:error, :no_path} -> scores
@@ -31,16 +25,55 @@ defmodule AdventOfCode.Solutions.Y24.Day10 do
     scores |> Map.values() |> Enum.sum()
   end
 
-  defp uphill_neighbors(pos, grid) do
-    altitude = Map.fetch!(grid, pos)
-    higher = altitude + 1
+  defp heads(grid), do: keys_for_altitude(grid, 0)
+  defp summits(grid), do: keys_for_altitude(grid, 9)
 
-    pos
-    |> Grid.cardinal4()
-    |> Enum.filter(&(Map.get(grid, &1) == higher))
+  defp keys_for_altitude(grid, altitude) do
+    Enum.flat_map(grid, fn
+      {k, ^altitude} -> [k]
+      _ -> []
+    end)
   end
 
-  # def part_two(problem) do
-  #   problem
-  # end
+  defp uphill_neighbors(pos, grid) do
+    uphill_neighbors(pos, grid, _altitude = Map.fetch!(grid, pos) + 1)
+  end
+
+  defp uphill_neighbors(pos, grid, altitude) do
+    pos
+    |> Grid.cardinal4()
+    |> Enum.filter(&(Map.get(grid, &1) == altitude))
+  end
+
+  def part_two(grid) do
+    scores =
+      for h <- heads(grid), s <- summits(grid), reduce: %{} do
+        scores ->
+          case Grid.bfs_path(grid, h, s, &uphill_neighbors/2) do
+            {:error, :no_path} ->
+              scores
+
+            {:ok, _} ->
+              rating = rating(grid, h, s)
+              Map.update(scores, h, rating, &(&1 + rating))
+          end
+      end
+
+    scores |> Map.values() |> Enum.sum()
+  end
+
+  defp rating(grid, head, summit) do
+    points =
+      Enum.reduce(1..9, [head], fn altitude, points ->
+        all_neighs(grid, points, altitude)
+      end)
+
+    Enum.count(points, &(&1 == summit))
+  end
+
+  defp all_neighs(grid, poses, altitude) do
+    Enum.flat_map(poses, fn pos ->
+      uphill_neighbors(pos, grid, altitude)
+    end)
+  end
 end
--- test/2024/day10_test.exs
+++ test/2024/day10_test.exs
@@ -33,21 +33,51 @@ defmodule AdventOfCode.Solutions.Y24.Day10Test do
     assert {:ok, @part_one_solution} == AoC.run(2024, 10, :part_one)
   end
 
-  # test "part two example" do
-  #   input = ~S"""
-  #   This is an
-  #   example input.
-  #   replace with
-  #   an example from
-  #   the AoC website.
-  #   """
-  #
-  #   assert CHANGE_ME == solve(input, :part_two)
-  # end
+  test "part two simple" do
+    input = ~S"""
+    .....0.
+    ..4321.
+    ..5..2.
+    ..6543.
+    ..7..4.
+    ..8765.
+    ..9....
+    """
 
-  # @part_two_solution CHANGE_ME
-  #
-  # test "part two solution" do
-  #   assert {:ok, @part_two_solution} == AoC.run(2024, 10, :part_two)
-  # end
+    assert 3 == solve(input, :part_two)
+  end
+
+  test "part two small" do
+    input = ~S"""
+    012345
+    123456
+    234567
+    345678
+    4.6789
+    56789.
+    """
+
+    assert 227 == solve(input, :part_two)
+  end
+
+  test "part two example" do
+    input = ~S"""
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+    """
+
+    assert 81 == solve(input, :part_two)
+  end
+
+  @part_two_solution 1483
+
+  test "part two solution" do
+    assert {:ok, @part_two_solution} == AoC.run(2024, 10, :part_two)
+  end
 end
