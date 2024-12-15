133e147e12d6e4c6d4f7ad99cde80a8d8566dac4
lud-wj <ludovic@demblans.com>
2024-12-15 11:28:40 +0100

    d15 ok

--- lib/solutions/2024/day15.ex
+++ lib/solutions/2024/day15.ex
@@ -39,8 +39,6 @@ defmodule AdventOfCode.Solutions.Y24.Day15 do
   def part_one({grid, moves, start}) do
     {grid, _} =
       Enum.reduce(moves, {grid, start}, fn direction, {grid, pos} ->
-        Grid.print(grid, &printer/1)
-
         case check_move(grid, direction, pos) do
           :blocked ->
             {grid, pos}
@@ -50,15 +48,8 @@ defmodule AdventOfCode.Solutions.Y24.Day15 do
         end
       end)
 
-    Grid.print(grid, &printer/1)
-
     calc_gps(grid)
   end
-
-  defp printer(nil), do: ?.
-  defp printer(:wall), do: ?#
-  defp printer(:crate), do: ?O
-  defp printer(:bot), do: ?@
 
   defp check_move(grid, direction, pos) do
     value = Map.fetch!(grid, pos)
@@ -83,18 +74,89 @@ defmodule AdventOfCode.Solutions.Y24.Day15 do
     deletions = Enum.map(changes, fn {prev, _} -> prev end)
     new_values = Map.new(changes, fn {_, xy_v} -> xy_v end)
 
-    grid = Map.drop(grid, deletions)
-    grid = Map.merge(grid, new_values)
+    grid
+    |> Map.drop(deletions)
+    |> Map.merge(new_values)
   end
 
   defp calc_gps(grid) do
     Enum.reduce(grid, 0, fn
       {{x, y}, :crate}, acc -> acc + x + 100 * y
+      {{x, y}, :CL}, acc -> acc + x + 100 * y
       _, acc -> acc
     end)
   end
 
-  # def part_two(problem) do
-  #   problem
-  # end
+  def part_two({grid, moves, start}) do
+    grid = expand_grid(grid)
+    {start, _} = expand_xy(start)
+
+    {grid, _} =
+      Enum.reduce(moves, {grid, start}, fn direction, {grid, pos} ->
+        case check_move_2(grid, direction, pos) do
+          :blocked ->
+            {grid, pos}
+
+          {:ok, [{_, {bot_pos, :bot}} | _] = changes} ->
+            {_grid = apply_moves(grid, changes), bot_pos}
+        end
+      end)
+
+    calc_gps(grid)
+  end
+
+  defp expand_grid(grid) do
+    grid
+    |> Enum.flat_map(fn {{x, y}, type} ->
+      {left, right} = expand_xy({x, y})
+
+      case type do
+        :crate -> [{left, :CL}, {right, :CR}]
+        :wall -> [{left, :wall}, {right, :wall}]
+        :bot -> [{left, :bot}]
+      end
+    end)
+    |> Map.new()
+  end
+
+  defp expand_xy({x, y}) do
+    x = x * 2
+    {{x, y}, {x + 1, y}}
+  end
+
+  defp check_move_2(_grid, _direction, :ignore_pos) do
+    {:ok, []}
+  end
+
+  defp check_move_2(grid, direction, pos) do
+    value = Map.fetch!(grid, pos)
+
+    next = Grid.translate(pos, direction)
+
+    case Map.get(grid, next) do
+      :wall ->
+        :blocked
+
+      nil ->
+        {:ok, [{pos, {next, value}}]}
+
+      crate when crate in [:CR, :CL] ->
+        other_half_pos = other_half(next, crate, direction)
+
+        with {:ok, changes} <- check_move_2(grid, direction, next),
+             {:ok, changes2} <- check_move_2(grid, direction, other_half_pos) do
+          {:ok, [{pos, {next, value}} | merge_changes(changes, changes2)]}
+        end
+
+    end
+  end
+
+  defp other_half(_pos, :CR, :e), do: :ignore_pos
+  defp other_half(pos, :CR, _), do: Grid.translate(pos, :w)
+  defp other_half(_pos, :CL, :w), do: :ignore_pos
+  defp other_half(pos, :CL, _), do: Grid.translate(pos, :e)
+
+  defp merge_changes(a, b) do
+    a ++ b
+  end
 end
--- test/2024/day15_test.exs
+++ test/2024/day15_test.exs
@@ -29,27 +29,88 @@ defmodule AdventOfCode.Solutions.Y24.Day15Test do
     assert 2028 == solve(input, :part_one)
   end
 
-  @part_one_solution 1486930
+  test "part one large" do
+    input = ~S"""
+    ##########
+    #..O..O.O#
+    #......O.#
+    #.OO..O.O#
+    #..O@..O.#
+    #O#..O...#
+    #O..O..O.#
+    #.OO.O.OO#
+    #....O...#
+    ##########
+
+    <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+    vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+    ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+    <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+    ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+    ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+    >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+    <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+    ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+    v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+    """
+
+    assert 10092 == solve(input, :part_one)
+  end
+
+  @part_one_solution 1_486_930
 
   test "part one solution" do
     assert {:ok, @part_one_solution} == AoC.run(2024, 15, :part_one)
   end
 
-  # test "part two example" do
-  #   input = ~S"""
-  #   This is an
-  #   example input.
-  #   replace with
-  #   an example from
-  #   the AoC website.
-  #   """
-  #
-  #   assert CHANGE_ME == solve(input, :part_two)
-  # end
+  test "part two check" do
+    input = ~S"""
+    #######
+    #...#.#
+    #.....#
+    #..OO@#
+    #..O..#
+    #.....#
+    #######
 
-  # @part_two_solution CHANGE_ME
-  #
-  # test "part two solution" do
-  #   assert {:ok, @part_two_solution} == AoC.run(2024, 15, :part_two)
-  # end
+    <vv<<^^<<^^
+    """
+
+    # should not raise
+    solve(input, :part_two)
+  end
+
+  test "part two example" do
+    input = ~S"""
+    ##########
+    #..O..O.O#
+    #......O.#
+    #.OO..O.O#
+    #..O@..O.#
+    #O#..O...#
+    #O..O..O.#
+    #.OO.O.OO#
+    #....O...#
+    ##########
+
+    <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+    vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+    ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+    <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+    ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+    ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+    >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+    <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+    ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+    v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+    """
+
+    assert 9021 == solve(input, :part_two)
+  end
+
+  @part_two_solution 1_492_011
+
+  test "part two solution" do
+    assert {:ok, @part_two_solution} == AoC.run(2024, 15, :part_two)
+  end
 end
