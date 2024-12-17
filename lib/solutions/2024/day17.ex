10139f34a9a20421e56a2b03190b4f30ef9fa400
lud-wj <ludovic@demblans.com>
2024-12-17 13:20:10 +0100

    p2 ok

--- lib/solutions/2024/day17.ex
+++ lib/solutions/2024/day17.ex
@@ -1,5 +1,4 @@
 defmodule AdventOfCode.Solutions.Y24.Day17 do
-  alias AdventOfCode.BinarySearch
   alias AoC.Input
 
   def parse(input, _part) do
@@ -76,6 +75,7 @@ defmodule AdventOfCode.Solutions.Y24.Day17 do
         :halt
 
       {:ok, {instr, arg}} ->
+        # print(instr,arg)
         # IO.puts("EXEC #{inspect(instr)} #{inspect(arg)}")
 
         result =
@@ -139,8 +139,8 @@ defmodule AdventOfCode.Solutions.Y24.Day17 do
   defp dv(dest, arg, state) do
     numerator = state.a
     divisor = 2 ** get_combo(state, arg)
-    dbg(numerator / divisor)
-    value = div(numerator, divisor) |> dbg()
+
+    value = div(numerator, divisor)
     state = putv(state, dest, value)
     {:next, state}
   end
@@ -152,225 +152,105 @@ defmodule AdventOfCode.Solutions.Y24.Day17 do
 
   def get_lit({v, _}), do: v
 
+  defp putv(state, :a, v) do
+    # IO.puts("UPDATE A !!!!! #{state.a} => #{v}")
+    %{state | a: v}
+  end
+
   defp putv(state, reg, v) when reg in [:a, :b, :c], do: %{state | reg => v}
 
   defp output(state, value), do: %{state | stdout: [value | state.stdout]}
 
   def part_two(vm) do
-    part_one(vm)
-    print_program(vm)
-    #   expected =
-    #     vm.raw
-    #     |> String.split(",")
-    #     |> Enum.map(&String.to_integer/1)
-    #     |> :lists.reverse()
-    #     |> dbg()
 
-    #   digit_count = length(expected)
-    #   digit_count |> IO.inspect(label: "digit_count")
+    # This may only work with my program...
+    #
+    #     B = A mod 8
+    #     B = B xor 3
+    #     C = A / 2 ** B
+    #     B = B xor 5
+    #     A = A / 2 ** 3
+    #     B = B xor C
+    #     puts B mod 8
+    #     if A != 0 then jump 0
+    #
+    # Starting from the end we can see that the only update to A is:
+    #
+    #     A = A / 2 ** 3      # 2 ** 3 = 8
+    #
+    # But as we are working with modulos (first line of fprogram starts with B =
+    # A mod 8), we need to consider all possible inputs between N and N+7 for a
+    # digit.
+    #
+    # We use the reversed output and iterate this way:
+    #
+    # * Start with possible inputs between 0 and 7, and index = 0 (index targets
+    #   the reversed expected output)
+    # * For each possible input, feed the program with that in the A registry,
+    #   and check that the output is the one exepected at `index`.
+    # * Keep only those values of A where the output was the expected one.
+    # * For each of those values, expand them to the possible modulo, so N
+    #   becomes N..(N+7).
+    # * That new list is the possible inputs for the next iteration, where we
+    #   will now look the output at index=1.
+    # * Loop over and over. The last loop is when index == number of digits - 1.
+    # * After the last loop the possible inputs list (96 numbers in my case)
+    #   contains the correct response.
+    # * There is probably a solution for that but since it's a small list we can
+    #   just run the program for each one until we find the solution.
 
-    #   #   [0 | target] = expected |> dbg()
-    #   #   target |> dbg()
-    #   #   t = Integer.undigits(target, 8) |> dbg()
-    #   #   (t * 8) |> dbg()
+    expected =
+      vm.raw
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+      |> :lists.reverse()
 
-    #   # I found that for a number in base 8, changing the nth digit of that number
-    #   # changes the nths digit of the (reverse) output. But not in order so we
-    #   # have to find what input digit outputs the right one.
+    targets = Map.new(Enum.with_index(expected), fn {d, i} -> {i, d} end)
 
-    #   base_state =
-    #     vm
-    #     |> Map.take([:a, :b, :c])
-    #     |> Map.put(:stdout, [])
+    digit_count = length(expected)
 
-    #   base_a = 8 ** (digit_count - 1)
-    #   base_a |> dbg()
-    #   IO.puts("base: 0o#{Integer.to_string(base_a, 8)}")
+    base_state =
+      vm
+      |> Map.take([:a, :b, :c])
+      |> Map.put(:stdout, [])
 
-    #   #   base
+    call = fn a ->
+      state = Map.put(base_state, :a, a)
+      %{stdout: stdout} = run(vm.program, state, 0)
+      stdout
+    end
 
-    #   # Enum.map(0..(digit_count - 1), fn index ->
-    #   #   target_digit = Enum.at(expected, index) |> dbg()
-    #   #   inputs =
+    possible_inputs = loop_find([0, 1, 2, 3, 4, 5, 6, 7], _index = 0, digit_count - 1, targets, call)
 
-    #   # end)
-
-    #   # expected |> Enum.map(&Map.fetch!(mapping, &1)) |> dbg()
-
-    #   empty_input = List.duplicate("1", digit_count)
-
-    #   # Enum.map(0..(digit_count - 1), fn index ->
-    #   #   IO.puts("======== index #{index}")
-    #   #   expected_digit = Enum.at(expected, index)
-    #   #   expected_digit |> IO.inspect(label: "expected_digit")
-    #   #   inputs = Enum.map(0..7, &{&1, List.replace_at(empty_input, index, Integer.to_string(&1))})
-
-    #   #   Enum.find_value(inputs, fn {digit, input} ->
-    #   #     IO.puts("------ digit #{digit} ")
-    #   #     ^digit_count = length(input)
-    #   #     {a, ""} = input |> Enum.join("") |> Integer.parse(8)
-    #   #     IO.puts("b8: " <> String.pad_leading(Integer.to_string(a, 8), digit_count, "0"))
-
-    #   #     state = Map.put(base_state, :a, a)
-
-    #   #     %{stdout: stdout} = run(vm.program, state, 0)
-    #   #     input |> IO.inspect(label: "input")
-    #   #     stdout |> IO.inspect(label: "stdout")
-
-    #   #     # if index == 0 && digit_count != length(stdout) do
-    #   #     #   {0, 0}
-    #   #     # else
-    #   #     if Enum.at(stdout, index) == expected_digit do
-    #   #       {index, digit}
-    #   #     else
-    #   #       false
-    #   #     end
-
-    #   #     # end
-    #   #   end)
-    #   # end)
-    #   # |> dbg()
-
-    #   # Enum.reduce((digit_count - 1)..0//-1, [], fn index, prev ->
-    #   #   IO.puts("======== index #{index}")
-    #   #   expected_digit = Enum.at(expected, index)
-    #   #   expected_digit |> IO.inspect(label: "expected_digit")
-    #   #   inputs = Enum.map(0..7, &{&1, [&1 | prev]})
-
-    #   #   Enum.find_value(inputs, fn {digit, input} ->
-    #   #     IO.puts("------ digit #{digit} ")
-    #   #     # ^digit_count = length(input)
-    #   #     {a, ""} = input |> Enum.join("") |> Integer.parse(8)
-    #   #     IO.puts("b8: " <> String.pad_leading(Integer.to_string(a, 8), digit_count, "0"))
-
-    #   #     state = Map.put(base_state, :a, a)
-
-    #   #     %{stdout: stdout} = run(vm.program, state, 0)
-    #   #     input |> IO.inspect(label: "input")
-    #   #     stdout |> IO.inspect(label: "stdout")
-
-    #   #     # if index == 0 && digit_count != length(stdout) do
-    #   #     #   {0, 0}
-    #   #     # else
-    #   #     case stdout do
-    #   #       [^expected_digit | _] -> {index, digit}
-    #   #       _ -> nil
-    #   #     end
-
-    #   #     # end
-    #   #   end)
-
-    #   #   # |> case do
-    #   #   #   {index, digit} -> {{index, digit}, [digit | prev]}
-    #   #   # end
-    #   # end)
-    #   # |> dbg()
-
-    #   # # Find the number of digits.
-    #   # # It's totally useless as it is the number
-    #   # pow =
-    #   #   BinarySearch.search(fn pow ->
-    #   #     a = 8 ** pow
-    #   #     state = Map.put(base_state, :a, 8 ** pow)
-
-    #   #     result = run(vm.program, state, 0) |> dbg()
-    #   #     a |> IO.inspect(label: "a")
-    #   #     len = length(result.stdout)
-
-    #   #     cond do
-    #   #       len > digit_count -> :gt
-    #   #       len < digit_count -> :lt
-    #   #       len == digit_count -> :eq
-    #   #     end
-    #   #   end)
-
-    #   # num_digits = pow + 1
-
-    #   #     # Stream.iterate(0, &(&1 + 1))
-
-    #   [
-    #     # 0o0,
-    #     # 0o1,
-    #     # 0o2,
-    #     # 0o3,
-    #     # 0o4,
-    #     # 0o5,
-    #     # 0o6,
-    #     # #
-    #     # 0o7,
-    #     # 0o10,
-    #     # 0o11,
-    #     # 0o12,
-    #     # 0o13,
-    #     # 0o14,
-    #     # 0o15,
-    #     # 0o16,
-    #     # #
-    #     # 0o17,
-    #     # 0o20,
-    #     # 0o21,
-    #     # 0o22,
-    #     # 0o23,
-    #     # 0o24,
-    #     # 0o25,
-    #     # 0o26,
-    #     # 0o27
-
-    #     0,
-    #     1,
-    #     2,
-    #     3,
-    #     4,
-    #     5,
-    #     6,
-    #     7,
-    #     0o10,
-    #     0o11,
-    #     0o12,
-    #     0o13,
-    #     0o14,
-    #     0o15,
-    #     0o16,
-    #     0o17,
-    #     0o20,
-    #     0o21,
-    #     0o22,
-    #     0o23,
-    #     0o24,
-    #     0o25,
-    #     0o26,
-    #     0o27
-    #   ]
-    #   # base_a..(base_a + 10)
-    #   |> Stream.filter(fn
-    #     :sep ->
-    #       IO.puts("----")
-    #       false
-
-    #     _ ->
-    #       true
-    #   end)
-    #   |> Stream.each(&IO.puts("b8: " <> Integer.to_string(&1, 8)))
-    #   |> Stream.map(fn a ->
-    #     state = Map.put(base_state, :a, a)
-    #     a |> IO.inspect(label: "a")
-
-    #     result = run(vm.program, state, 0)
-    #     result.stdout |> IO.inspect(label: "result.stdout")
-
-    #     if result.stdout == expected do
-    #       throw({:found, a})
-    #     end
-    #   end)
-    #   |> Stream.run()
-
-    #   expected |> IO.inspect(label: "expected")
-    #   :not_found
-    # catch
-    #   {:found, a} -> a
+    Enum.find(possible_inputs, fn a -> call.(a) == expected end)
   end
 
-  defp print_program(vm) do
+  defp loop_find(inputs, index, max_pow, targets, call) when index < max_pow do
+    target = Map.fetch!(targets, index)
+
+    next_inputs =
+      inputs
+      |> Enum.filter(fn a ->
+        stdout = call.(a)
+        Enum.at(stdout, index) == target
+      end)
+      |> Enum.flat_map(fn a ->
+        next = a * 8
+        range = next..(next + 7)
+        Enum.filter(range, &(div(&1, 8) == a))
+      end)
+
+    loop_find(next_inputs, index + 1, max_pow, targets, call)
+  end
+
+  defp loop_find(inputs, _index, _max_pow, _targets, _call) do
+    inputs
+  end
+
+
+  # Unused but helpful !
+
+  def print_program(vm) do
     %{program: program} = vm
 
     program
@@ -390,13 +270,12 @@ defmodule AdventOfCode.Solutions.Y24.Day17 do
 
   # 1
   defp print(:bxl, arg) do
-    IO.puts "B = B xor #{print_lit(arg)}"
-
+    IO.puts("B = B xor #{print_lit(arg)}")
   end
 
   # 2
   defp print(:bst, arg) do
-    IO.puts "B = #{print_combo(arg)} mod 8"
+    IO.puts("B = #{print_combo(arg)} mod 8")
   end
 
   # 3
@@ -406,7 +285,7 @@ defmodule AdventOfCode.Solutions.Y24.Day17 do
 
   # 4
   defp print(:bxc, _arg) do
-    IO.puts "B = B xor C"
+    IO.puts("B = B xor C")
   end
 
   # 5
