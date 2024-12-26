aa0317fe5fa174e00aad93d4764265bdb935bdc3
lud-wj <ludovic@demblans.com>
2024-12-26 15:10:49 +0100

    refacto

--- lib/solutions/2024/day25.ex
+++ lib/solutions/2024/day25.ex
@@ -14,32 +14,14 @@ defmodule AdventOfCode.Solutions.Y24.Day25 do
   defp parse_block("#####\n" <> _ = lock), do: {:locks, parse_lock(lock)}
   defp parse_block(".....\n" <> _ = key), do: {:keys, parse_key(key)}
 
-  _ =
-    defp parse_lock(lines) do
-      coords = to_coords(lines)
-
-      for x <- 0..4 do
-        coords
-        |> Enum.max_by(fn
-          {^x, y} -> y
-          _ -> -1
-        end)
-        |> elem(1)
-      end
-    end
+  defp parse_lock(lines) do
+    coords = to_coords(lines)
+    Enum.map(0..4, fn x -> Enum.max(Map.fetch!(coords, x)) end)
+  end
 
   defp parse_key(lines) do
     coords = to_coords(lines)
-
-    for x <- 0..4 do
-      6 -
-        (coords
-         |> Enum.min_by(fn
-           {^x, y} -> y
-           _ -> 9999
-         end)
-         |> elem(1))
-    end
+    Enum.map(0..4, fn x -> 6 - Enum.min(Map.fetch!(coords, x)) end)
   end
 
   defp to_coords(lines) do
@@ -51,24 +33,16 @@ defmodule AdventOfCode.Solutions.Y24.Day25 do
     end)
     |> elem(0)
     |> Map.keys()
+    |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
   end
 
   def part_one(%{locks: locks, keys: keys}) do
     for lock <- locks, key <- keys, reduce: 0 do
-      acc ->
-        if opens?(lock, key) do
-          acc + 1
-        else
-          acc
-        end
+      acc -> if opens?(lock, key), do: acc + 1, else: acc
     end
   end
 
   defp opens?([p | lock], [k | key]) when p + k < 6, do: opens?(lock, key)
-  defp opens?([p | _], [k | _]) when p + k >= 6, do: false
+  defp opens?([_ | _], [_ | _]), do: false
   defp opens?([], []), do: true
-
-  # def part_two(problem) do
-  #   problem
-  # end
 end
