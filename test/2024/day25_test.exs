c9ba0f7b86a79ab5dfd552bd235bbba6288271ec
lud-wj <ludovic@demblans.com>
2024-12-26 15:06:04 +0100

    d25

---
+++ test/2024/day25_test.exs
@@ -0,0 +1,66 @@
+defmodule AdventOfCode.Solutions.Y24.Day25Test do
+  alias AoC.Input, warn: false
+  alias AdventOfCode.Solutions.Y24.Day25, as: Solution, warn: false
+  use ExUnit.Case, async: true
+
+  defp solve(input, part) do
+    problem =
+      input
+      |> Input.as_file()
+      |> Solution.parse(part)
+
+    apply(Solution, part, [problem])
+  end
+
+  test "part one example" do
+    input = ~S"""
+    #####
+    .####
+    .####
+    .####
+    .#.#.
+    .#...
+    .....
+
+    #####
+    ##.##
+    .#.##
+    ...##
+    ...#.
+    ...#.
+    .....
+
+    .....
+    #....
+    #....
+    #...#
+    #.#.#
+    #.###
+    #####
+
+    .....
+    .....
+    #.#..
+    ###..
+    ###.#
+    ###.#
+    #####
+
+    .....
+    .....
+    .....
+    #....
+    #.#..
+    #.#.#
+    #####
+    """
+
+    assert 3 == solve(input, :part_one)
+  end
+
+  @part_one_solution 3344
+
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2024, 25, :part_one)
+  end
+end
