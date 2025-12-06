7a23073eb9fa16c3328c98c40386273edb89661b
lud-wj <ludovic@demblans.com>
2025-12-06 10:41:02 +0100

    d6

---
+++ lib/solutions/2025/day06.ex
@@ -0,0 +1,47 @@
+defmodule AdventOfCode.Solutions.Y25.Day06 do
+  alias AoC.Input
+
+  # No separate parsing today :)
+  def parse(input, _), do: input
+
+  def part_one(input) do
+    input
+    |> Input.stream!(trim: true)
+    |> Stream.map(&String.split(&1, " ", trim: true))
+    |> Enum.zip_with(& &1)
+    |> Enum.sum_by(fn list ->
+      {op, args} = List.pop_at(list, -1)
+      apply_op(op, Enum.map(args, &String.to_integer/1))
+    end)
+  end
+
+  def part_two(input) do
+    rows = Input.read!(input) |> String.split("\n", trim: true)
+    {ops, rows} = List.pop_at(rows, -1)
+    rows = Enum.map(rows, &String.graphemes/1)
+    ops = String.split(ops, " ", trim: true)
+
+    rows
+    |> Stream.unfold(fn
+      [[] | _] ->
+        nil
+
+      rows ->
+        {rows, col} = Enum.map_reduce(rows, [], fn [h | t], acc -> {t, [h | acc]} end)
+        {:lists.reverse(col), rows}
+    end)
+    |> Stream.map(fn str_digits ->
+      case str_digits |> Enum.join("") |> String.trim() do
+        "" -> nil
+        str_num -> String.to_integer(str_num)
+      end
+    end)
+    |> Stream.chunk_by(&is_integer/1)
+    |> Stream.filter(&(&1 != [nil]))
+    |> Stream.zip(ops)
+    |> Enum.sum_by(fn {numbers, op} -> apply_op(op, numbers) end)
+  end
+
+  defp apply_op("*", args), do: Enum.reduce(args, &Kernel.*/2)
+  defp apply_op("+", args), do: Enum.reduce(args, &Kernel.+/2)
+end
---
+++ test/2025/day06_test.exs
@@ -0,0 +1,53 @@
+defmodule AdventOfCode.Solutions.Y25.Day06Test do
+  alias AoC.Input, warn: false
+  alias AdventOfCode.Solutions.Y25.Day06, as: Solution, warn: false
+  use ExUnit.Case, async: true
+
+  defp solve(input, part) do
+    problem =
+      input
+      |> Input.as_file()
+      |> Solution.parse(part)
+
+    apply(Solution, part, [problem])
+  end
+
+  test "part one example" do
+    input = ~S"""
+    123 328  51 64
+     45 64  387 23
+      6 98  215 314
+    *   +   *   +
+    """
+
+    assert 4_277_556 == solve(input, :part_one)
+  end
+
+  @part_one_solution 5_060_053_676_136
+
+  test "part one solution" do
+    assert {:ok, @part_one_solution} == AoC.run(2025, 6, :part_one)
+  end
+
+  test "part two example" do
+    # Actual input is aligned on the right, so I changed the example to be
+    # aligned on the right too. Makes everything simpler.
+    #
+    # Operators do not need to be aligned.
+
+    input = ~S"""
+    123 328  51  64
+     45 64  387  23
+      6 98  215 314
+    *   +   *   +
+    """
+
+    assert 3_263_827 == solve(input, :part_two)
+  end
+
+  @part_two_solution 9_695_042_567_249
+
+  test "part two solution" do
+    assert {:ok, @part_two_solution} == AoC.run(2025, 6, :part_two)
+  end
+end
