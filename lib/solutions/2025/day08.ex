2d80729ccba87d38e57accdb104cc6ea8f0851d8
lud-wj <ludovic@demblans.com>
2025-12-08 13:05:08 +0100

    no more golfing

--- lib/solutions/2025/day08.ex
+++ lib/solutions/2025/day08.ex
@@ -9,30 +9,23 @@ defmodule AdventOfCode.Solutions.Y25.Day08 do
 
   defp parse_line(line) do
     [x, y, z] = String.split(line, ",")
-
-    {
-      String.to_integer(x),
-      String.to_integer(y),
-      String.to_integer(z)
-    }
+    {String.to_integer(x), String.to_integer(y), String.to_integer(z)}
   end
 
   def part_one(xyzs, n_pairs \\ 1000) do
-    circuits = Map.new(Enum.with_index(xyzs))
+    circuits = Map.new(Enum.with_index(xyzs), fn {xyz, circ} -> {circ, MapSet.new([xyz])} end)
 
     xyzs
     |> all_distances()
     |> Enum.take(n_pairs)
     |> Enum.reduce(circuits, fn {{a, b}, _}, circuits -> merge_circuits(circuits, a, b) end)
-    |> Enum.group_by(&elem(&1, 1))
-    |> Enum.map(fn {_, list} -> length(list) end)
+    |> Enum.map(fn {_, set} -> MapSet.size(set) end)
     |> Enum.sort(:desc)
-    |> Enum.take(3)
-    |> Enum.product()
+    |> then(fn [a, b, c | _] -> a * b * c end)
   end
 
   def part_two(xyzs, _ \\ nil) do
-    circuits = Map.new(Enum.with_index(xyzs))
+    circuits = Map.new(Enum.with_index(xyzs), fn {xyz, circ} -> {circ, MapSet.new([xyz])} end)
 
     xyzs
     |> all_distances()
@@ -46,33 +39,40 @@ defmodule AdventOfCode.Solutions.Y25.Day08 do
   end
 
   defp all_distances(xyzs) do
-    all_distances = for a <- xyzs, b <- xyzs, a != b, into: %{}, do: {{min(a, b), max(a, b)}, distance(a, b)}
-    Enum.sort(all_distances, fn {_, dist_a}, {_, dist_b} -> dist_a <= dist_b end)
+    {_time, dists} = :timer.tc(fn -> do_all_distances(xyzs, []) end, :millisecond)
+    # time |> IO.inspect(limit: :infinity, label: "time")
+    dists
+  end
+
+  defp do_all_distances([h | t], acc) do
+    acc = Enum.reduce(t, acc, &[{{h, &1}, distance(h, &1)} | &2])
+    do_all_distances(t, acc)
+  end
+
+  defp do_all_distances([], acc) do
+    Enum.sort(acc, fn {_, dist_a}, {_, dist_b} -> dist_a <= dist_b end)
   end
 
   defp merge_circuits(circuits, xyz_a, xyz_b) do
-    circ_a = Map.fetch!(circuits, xyz_a)
-    circ_b = Map.fetch!(circuits, xyz_b)
+    {circ_a, group_a} = Enum.find(circuits, fn {_, items} -> xyz_a in items end)
 
-    Map.new(circuits, fn
-      {xyz, ^circ_a} -> {xyz, circ_b}
-      other -> other
-    end)
+    case Enum.find(circuits, fn {_, items} -> xyz_b in items end) do
+      {^circ_a, _} ->
+        circuits
+
+      {circ_b, group_b} ->
+        circuits
+        |> Map.put(circ_b, MapSet.union(group_a, group_b))
+        |> Map.delete(circ_a)
+    end
   end
 
   defp distance({xa, ya, za}, {xb, yb, zb}) do
-    :math.sqrt(
-      :math.pow(xb - xa, 2) +
-        :math.pow(yb - ya, 2) +
-        :math.pow(zb - za, 2)
-    )
+    (xb - xa) ** 2 + (yb - ya) ** 2 + (zb - za) ** 2
   end
 
   defp single_circuit?(map) do
-    case Enum.uniq(Map.values(map)) do
-      [_, _ | _] -> false
-      [_] -> true
-    end
+    map_size(map) == 1
   end
 
   defp x({x, _, _}), do: x
