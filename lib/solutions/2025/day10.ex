3ab6341ed0d34f07c5b48a5d50a9bf20002f591f
lud-wj <ludovic@demblans.com>
2025-12-10 18:27:06 +0100

    brute force not working

--- lib/solutions/2025/day10.ex
+++ lib/solutions/2025/day10.ex
@@ -27,7 +27,6 @@ defmodule AdventOfCode.Solutions.Y25.Day10 do
       Enum.map(buttons, fn str ->
         str |> String.split(",") |> Enum.map(&String.to_integer/1)
       end)
-      |> Enum.sort()
 
     jolts =
       jolts
@@ -96,7 +95,110 @@ defmodule AdventOfCode.Solutions.Y25.Day10 do
     end)
   end
 
-  def part_two(problem) do
-    problem
+  def part_two(machines) do
+    machines
+    |> Enum.with_index(1)
+    # |> :lists.reverse()
+    |> Enum.sum_by(fn {machine, index} -> joltage_press(machine, index) end)
+  end
+
+  defp joltage_press({_, buttons, target}, machine_index) do
+    size = length(target)
+    buttons |> dbg()
+    target |> dbg()
+    min_press = Enum.max(target)
+    init_state = {Map.new(buttons, &button_to_incrementors(&1, target)), 0} |> dbg()
+    loop(_states = [init_state], target, min_press, 1, machine_index)
+  catch
+    {:found, n} -> n
+  end
+
+  defp button_to_incrementors(indices, target) do
+    increments = for(i <- 0..(length(target) - 1), do: if(i in indices, do: 1, else: 0))
+
+    # the minimum value that is touched by this button becomes the max allowed presses
+    target |> dbg()
+
+    max_press =
+      indices
+      |> Enum.map(&Enum.at(target, &1))
+      |> Enum.min()
+
+    {increments, {max_press, _pressed = 0}}
+  end
+
+  defp loop([], _, _, _, _) do
+    raise "no more states"
+  end
+
+  defp loop(states, target, min_press, iter, machine_index) do
+    IO.puts("-- #{machine_index} / #{iter} -----------------------")
+    states = Enum.flat_map(states, &press_to_new_states(&1, target, min_press))
+    # IO.puts("length before uniq: #{length(states)}")
+    states = Enum.uniq(states)
+    IO.puts("length after uniq: #{length(states)}")
+    # states = Enum.take(Enum.sort_by(states, &heuristic(&1, target)), 30_000)
+    # IO.puts("length after heuristic: #{length(states)}")
+    loop(states, target, min_press, iter + 1, machine_index)
+  end
+
+  # returns all next states for a state, one for each pressable button
+  defp press_to_new_states(state, target, min_press) do
+    {buttons, total_press} = state
+
+    Enum.flat_map(buttons, fn
+      # maxed out
+      {button, {max_press, max_press}} ->
+        []
+
+      {button, {max_press, pressed}} ->
+        new_state = {Map.put(buttons, button, {max_press, pressed + 1}), total_press + 1}
+
+        case classify(new_state, target, min_press) do
+          :overshoot -> []
+          :too_low -> [new_state]
+          :equal -> throw({:found, total_press + 1})
+        end
+    end)
+  end
+
+  defp classify({map, n} = state, target, min_press) when n < min_press do
+    vals = state_value(state)
+    comparison = Enum.zip(vals, target)
+
+    if Enum.any?(comparison, fn {v, t} -> v > t end) do
+      :overshoot
+    else
+      :too_low
+    end
+  end
+
+  defp classify(state, target, min_press) do
+    vals = state_value(state)
+
+    if vals == target do
+      :equal
+    else
+      comparison = Enum.zip(vals, target)
+
+      if Enum.any?(comparison, fn {v, t} -> v > t end) do
+        :overshoot
+      else
+        :too_low
+      end
+    end
+  end
+
+  defp heuristic(state, target) do
+    Enum.zip_with([target, state_value(state)], fn [t, v] ->
+      t / (t - v + 1)
+    end)
+    |> Enum.sum()
+  end
+
+  defp state_value({map, _}) do
+    map
+    |> Enum.map(fn {buttons, {_, presses}} -> Enum.map(buttons, &(&1 * presses)) end)
+    |> Enum.zip_with(&Enum.sum/1)
   end
 end
