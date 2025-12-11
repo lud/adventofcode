d0092f4c8e0b7c3e1ce4f9a59c93fd6c842a3223
lud-wj <ludovic@demblans.com>
2025-12-11 11:38:34 +0100

    p2 ok

--- lib/solutions/2025/day11.ex
+++ lib/solutions/2025/day11.ex
@@ -22,74 +22,42 @@ defmodule AdventOfCode.Solutions.Y25.Day11 do
   end
 
   def part_one(full_io) do
-    # out_in =
-    #   Enum.reduce(full_io, %{}, fn {inp, outs}, acc ->
-    #     Enum.reduce(outs, acc, fn out, acc ->
-    #       Map.update(acc, out, [inp], &[inp | &1])
-    #     end)
-    #   end)
-
-    # lead_to_out = exit_paths(out_in, ["out"], %{})
-    # paths = build_paths(full_io,"you",%{})
-    counts = count_paths(full_io, "you", %{"out" => 1})
-    Map.fetch!(counts, "you")
+    count_paths(full_io, "you", "out")
   end
 
-  defp count_paths(full_io, "out", acc) do
-    raise "not called"
+  defp count_paths(full_io, start, finish) do
+    counts = count_paths_loop(full_io, start, finish, Map.put(%{"out" => 0}, finish, 1))
+    Map.fetch!(counts, start)
   end
 
-  defp count_paths(full_io, todo, acc) do
-    todo |> dbg()
-    exits = Map.fetch!(full_io, todo)
-    exits |> dbg()
+  defp count_paths_loop(full_io, current, finish, acc) do
+    exits = Map.fetch!(full_io, current)
 
     acc =
       Enum.reduce(exits, acc, fn exit_, acc ->
-        acc =
-          if Map.has_key?(acc, exit_) do
-            acc
-          else
-            count_paths(full_io, exit_, acc)
-          end
+        if Map.has_key?(acc, exit_) do
+          acc
+        else
+          count_paths_loop(full_io, exit_, finish, acc)
+        end
       end)
 
     value = Enum.sum_by(exits, &Map.fetch!(acc, &1))
 
-    Map.put(acc, todo, value)
+    Map.put(acc, current, value)
   end
-
-  # defp build_paths(full_io,current, acc ) do
-  #   outs = Map.fetch!(full_io,current)
-  #   Map.put
-  # end
 
   def part_two(full_io) do
-    paths = walk(full_io, [["svr"]], 0)
-  end
+    {step1, step2} =
+      if count_paths(full_io, "fft", "dac") > 0 do
+        {"fft", "dac"}
+      else
+        true = count_paths(full_io, "dac", "fft") > 0
+        {"dac", "fft"}
+      end
 
-  defp walk(_, [], count) do
-    count
-  end
-
-  defp walk(full_io, states, count) do
-    {states, count} =
-      Enum.flat_map_reduce(states, count, fn [h | _] = path, count ->
-        Enum.flat_map_reduce(Map.fetch!(full_io, h), count, fn
-          "out", count ->
-            if "fft" in path and "dac" in path do
-              {[], count + 1}
-            else
-              {[], count}
-            end
-
-          exit_, count ->
-            {[[exit_ | path]], count}
-        end)
-      end)
-
-    length(states) |> dbg(limit: :infinity)
-
-    walk(full_io, states, count)
+    count_paths(full_io, "svr", step1) *
+      count_paths(full_io, step1, step2) *
+      count_paths(full_io, step2, "out")
   end
 end
