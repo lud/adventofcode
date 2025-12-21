a5d85d74a0a85719b2ae04b89e3c845ab90fbcc5
lud-wj <ludovic@demblans.com>
2025-12-21 16:08:52 +0100

    refacto p1

---
+++ lib/solutions/2019/day16.ex
@@ -0,0 +1,122 @@
+defmodule AdventOfCode.Solutions.Y19.Day16 do
+  alias AoC.Input
+
+  def parse(input, _part) do
+    input |> Input.read!() |> String.trim() |> String.to_integer() |> Integer.digits()
+  end
+
+  @base_pattern [0, 1, 0, -1]
+
+  def part_one(digits, n_phases \\ 100) do
+    size = length(digits)
+
+    1..n_phases
+    |> Enum.reduce(digits, fn i, digits ->
+      IO.puts("phase: #{i}")
+      _digits = apply_fft(digits, size)
+    end)
+    |> Enum.take(8)
+    |> Integer.undigits()
+  end
+
+  defp apply_fft(digits, size) do
+    digits
+    |> Enum.with_index(1)
+    |> Enum.map(fn {_, pattern_repeats} ->
+      if rem(pattern_repeats, 100) == 0 do
+        pattern_repeats |> IO.inspect(limit: :infinity, label: "pattern_repeats")
+      end
+
+      value_of(digits, 1, pattern_repeats, 0)
+      # |> IO.inspect(limit: :infinity, label: "digit #{pattern_repeats}")
+    end)
+  end
+
+  # pattern_repeats the the digit we are trying to compute, which determines the
+  # pattern repetitions
+  defp value_of([h | t], nth_digit, pattern_repeats, acc) do
+    # IO.write("#{h}*#{nth_pattern(nth_digit, pattern_repeats)} + ")
+    acc = acc + h * nth_pattern(nth_digit, pattern_repeats)
+    value_of(t, nth_digit + 1, pattern_repeats, acc)
+  end
+
+  defp value_of([], _nth_digit, _pattern_repeats, acc) do
+    v = rem(abs(acc), 10)
+    # v |> IO.inspect(limit: :infinity, label: "=>")
+    v
+  end
+
+  def nth_pattern(nth_digit, pattern_repeats) do
+    # if nth_digit is `1` we will take the first item of the list, so actually
+    # removing the first item of the pattern (as in the rules) counterbalances
+    # the need to have modulo==0 for the numbers, so the remainder is directly
+    # the correct position
+
+    # nth_digit |> dbg()
+    # pattern_repeats |> dbg()
+    pattern_length = 4 * pattern_repeats
+    # pattern_length |> dbg()
+
+    # if nth_digit=1, pattern_repeats=1
+    #     pattern is (0),[1],0,-1
+    #     we want 1
+    # if nth_digit=1, pattern_repeats=2
+    #     pattern is (0),[0],1,1,0,0,-1,-1
+    #     we want 0
+    # if nth_digit=1, pattern_repeats=3
+    #     pattern is (0),[0],0,1,1,1,0,0,0,-1,-1,-1
+    #     we want 1
+    # if nth_digit=4, pattern_repeats=1
+    #     pattern is (0),1,0,-1,[0]
+    #     we want 1
+
+    # if nth_digit=5, pattern_repeats=1
+    #     pattern is (0),1,0,-1,0,[1],0,-1
+    #     we want 1
+    # if nth_digit=5, pattern_repeats=2
+    #     pattern is (0),0,1,1,0,[0],-1,-1,0,0
+    #     we want 0
+    # if nth_digit=5, pattern_repeats=3
+    #     pattern is (0),0,0,1,1,[1],0,0,0,-1,-1,-1,0,0,0
+    #     we want 1
+
+    pattern_index = rem(nth_digit, pattern_length)
+    # pattern_index |> dbg()
+
+    cond do
+      pattern_repeats > pattern_index -> 0
+      pattern_repeats * 2 > pattern_index -> 1
+      pattern_repeats * 3 > pattern_index -> 0
+      pattern_repeats * 4 > pattern_index -> -1
+    end
+  end
+
+  # |> tap(
+  #   &(&1
+  #     |> Integer.undigits()
+  #     |> Integer.to_string()
+  #     |> String.pad_leading(32, "0")
+  #     |> IO.puts())
+  # )
+
+  defp make_pattern(size, nth_digit) do
+    [h | t] = full_pattern = Enum.flat_map(@base_pattern, fn d -> List.duplicate(d, nth_digit) end)
+    # The first repetition must ignore the first number
+    Stream.concat(t, Stream.cycle(full_pattern))
+  end
+
+  def part_two(digits) do
+    size = length(digits)
+    digits = Enum.flat_map(1..10000, fn _ -> digits end)
+    size = size * 10000
+    true = size == length(digits)
+
+    1..100
+    |> Enum.reduce(digits, fn i, digits ->
+      IO.puts("phase: #{i}")
+      apply_fft(digits, size)
+    end)
+    |> Enum.take(8)
+    |> Integer.undigits()
+  end
+end
---
+++ test/2019/day16_test.exs
@@ -0,0 +1,130 @@
+defmodule AdventOfCode.Solutions.Y19.Day16Test do
+  alias AoC.Input, warn: false
+  alias AdventOfCode.Solutions.Y19.Day16, as: Solution, warn: false
+  use ExUnit.Case, async: true
+
+  defp solve(input, part, n_phases \\ 100) do
+    problem =
+      input
+      |> Input.as_file()
+      |> Solution.parse(part)
+
+    apply(Solution, part, [problem, n_phases])
+  end
+
+  test "get the nth_pattern element" do
+    # get the 5 first digits to compute the first digit of the signal
+    #
+    # on first digit, pattern is the following (parenthesis show the skipped head
+    # on first repetition)
+    #
+    #     (0),[1,0,-1,0,1],0,-1,0,1,0,-1,0,1,0,-1,
+    #
+    # So we should get 1,0,-1,0,1
+    pattern_repeats = 1
+
+    assert 1 = Solution.nth_pattern(1, pattern_repeats)
+    assert 0 = Solution.nth_pattern(2, pattern_repeats)
+    assert -1 = Solution.nth_pattern(3, pattern_repeats)
+    assert 0 = Solution.nth_pattern(4, pattern_repeats)
+    assert 1 = Solution.nth_pattern(5, pattern_repeats)
+
+    # 5 first digits for the 2nd digit position, pattern is:
+    #
+    #     (0),[0,1,1,0,0],-1,-10,0,1,1,0,0,-1,-10,0,1,1,0,0,-1,-10,0,1,1,0,0,-1,-1
+    pattern_repeats = 2
+
+    assert 0 = Solution.nth_pattern(1, pattern_repeats)
+    assert 1 = Solution.nth_pattern(2, pattern_repeats)
+    assert 1 = Solution.nth_pattern(3, pattern_repeats)
+    assert 0 = Solution.nth_pattern(4, pattern_repeats)
+    assert 0 = Solution.nth_pattern(5, pattern_repeats)
+
+    # 5 first digits for the 3rd digit position, pattern is:
+    #
+    # (0),[0,0,1,1,1],0,0,0,-1,-1,-1,0,0,0,1,1,1,0,0,0,-1,-1,-1,0,0,0,1,1,1,0,0,0,-
+    pattern_repeats = 3
+
+    assert 0 = Solution.nth_pattern(1, pattern_repeats)
+    assert 0 = Solution.nth_pattern(2, pattern_repeats)
+    assert 1 = Solution.nth_pattern(3, pattern_repeats)
+    assert 1 = Solution.nth_pattern(4, pattern_repeats)
+    assert 1 = Solution.nth_pattern(5, pattern_repeats)
+  end
+
+  test "get farther elements" do
+    pattern_repeats = 1
+
+    assert 1 = Solution.nth_pattern(1, pattern_repeats)
+    assert 0 = Solution.nth_pattern(2, pattern_repeats)
+    assert -1 = Solution.nth_pattern(3, pattern_repeats)
+    assert 0 = Solution.nth_pattern(4, pattern_repeats)
+    assert 1 = Solution.nth_pattern(5, pattern_repeats)
+    assert 0 = Solution.nth_pattern(6, pattern_repeats)
+    assert -1 = Solution.nth_pattern(7, pattern_repeats)
+    assert 0 = Solution.nth_pattern(8, pattern_repeats)
+
+    pattern_repeats = 2
+
+    assert 0 = Solution.nth_pattern(1, pattern_repeats)
+    assert 1 = Solution.nth_pattern(2, pattern_repeats)
+    assert 1 = Solution.nth_pattern(3, pattern_repeats)
+    assert 0 = Solution.nth_pattern(4, pattern_repeats)
+    assert 0 = Solution.nth_pattern(5, pattern_repeats)
+    assert -1 = Solution.nth_pattern(6, pattern_repeats)
+    assert -1 = Solution.nth_pattern(7, pattern_repeats)
+    assert 0 = Solution.nth_pattern(8, pattern_repeats)
+
+    pattern_repeats = 3
+
+    assert 0 = Solution.nth_pattern(1, pattern_repeats)
+    assert 0 = Solution.nth_pattern(2, pattern_repeats)
+    assert 1 = Solution.nth_pattern(3, pattern_repeats)
+    assert 1 = Solution.nth_pattern(4, pattern_repeats)
+    assert 1 = Solution.nth_pattern(5, pattern_repeats)
+    assert 0 = Solution.nth_pattern(6, pattern_repeats)
+    assert 0 = Solution.nth_pattern(7, pattern_repeats)
+    assert 0 = Solution.nth_pattern(8, pattern_repeats)
+  end
+
+  test "basic test" do
+    assert 01_029_498 = solve("12345678", :part_one, 4)
+  end
+
+  @tag :skip
+  test "part one example" do
+    assert 24_176_176 == solve("80871224585914546619083218645595", :part_one)
+    assert 73_745_418 == solve("19617804207202209144916044189917", :part_one)
+    assert 52_432_133 == solve("69317163492948606335995924319873", :part_one)
+
+    flunk("ok so far")
+  end
+
+  # test "does it repeat?" do
+  #   assert 24_176_176 == solve("80871224585914546619083218645595", :part_one)
+  # end
+
+  # @part_one_solution CHANGE_ME
+  #
+  # test "part one solution" do
+  #   assert {:ok, @part_one_solution} == AoC.run(2019, 16, :part_one)
+  # end
+
+  # test "part two example" do
+  #   input = ~S"""
+  #   This is an
+  #   example input.
+  #   replace with
+  #   an example from
+  #   the AoC website.
+  #   """
+  #
+  #   assert CHANGE_ME == solve(input, :part_two)
+  # end
+
+  # @part_two_solution CHANGE_ME
+  #
+  # test "part two solution" do
+  #   assert {:ok, @part_two_solution} == AoC.run(2019, 16, :part_two)
+  # end
+end
